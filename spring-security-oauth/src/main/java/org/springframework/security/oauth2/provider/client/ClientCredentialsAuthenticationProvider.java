f6b5bdcba3cfea87d178a77fcb2d124a113592f8
Ryan Heaton <ryan@webcohesion.com>
2011-03-11 15:50:05 -0700

    forgot to add the client credentials provider and token

---
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth2/provider/client/ClientCredentialsAuthenticationProvider.java
@@ -0,0 +1,41 @@
+package org.springframework.security.oauth2.provider.client;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.password.ClientPasswordAuthenticationToken;
+import org.springframework.util.Assert;
+
+public class ClientCredentialsAuthenticationProvider implements AuthenticationProvider, InitializingBean {
+
+	  private AuthenticationManager authenticationManager;
+
+	  public void afterPropertiesSet() throws Exception {
+	    Assert.notNull(this.authenticationManager, "An authentication manager must be provided.");
+	  }
+
+	  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+	    ClientCredentialsAuthenticationToken auth = (ClientCredentialsAuthenticationToken) authentication;
+	    Authentication clientAuth = getAuthenticationManager().authenticate(auth.getClientAuthentication());
+	    return new OAuth2Authentication(clientAuth, null);
+	  }
+
+	  public boolean supports(Class<?> authentication) {
+	    return ClientCredentialsAuthenticationToken.class.isAssignableFrom(authentication);
+	  }
+
+	  public AuthenticationManager getAuthenticationManager() {
+	    return authenticationManager;
+	  }
+
+	  @Autowired
+	  public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+	    this.authenticationManager = authenticationManager;
+	  }
+	
+
+}
