f32631fee9ecd6b5c2fd865a2cd884101891619b
Dave Syer <dsyer@vmware.com>
2011-07-28 13:09:58 +0100

    SECOAUTH-68: switch tonr2 to @MVC

--- tonr2/src/main/java/org/springframework/security/oauth/examples/tonr/mvc/FacebookController.java
+++ tonr2/src/main/java/org/springframework/security/oauth/examples/tonr/mvc/FacebookController.java
@@ -1,74 +1,71 @@
 package org.springframework.security.oauth.examples.tonr.mvc;
 
+import java.util.ArrayList;
+
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
-import org.springframework.security.oauth2.consumer.*;
+import org.springframework.security.oauth2.consumer.OAuth2AccessTokenRequiredException;
+import org.springframework.security.oauth2.consumer.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.consumer.OAuth2RestTemplate;
+import org.springframework.security.oauth2.consumer.OAuth2SecurityContext;
+import org.springframework.security.oauth2.consumer.OAuth2SecurityContextHolder;
+import org.springframework.security.oauth2.consumer.OAuth2SecurityContextImpl;
 import org.springframework.security.oauth2.consumer.token.OAuth2ClientTokenServices;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 
 /**
  * @author Ryan Heaton
+ * @author Dave Syer
  */
-public class FacebookController extends AbstractController {
+@Controller
+public class FacebookController {
 
-  private OAuth2RestTemplate facebookRestTemplate;
-  private OAuth2ClientTokenServices tokenServices;
+	private OAuth2RestTemplate facebookRestTemplate;
+	private OAuth2ClientTokenServices tokenServices;
 
-  protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    try {
-      ObjectNode result = getFacebookRestTemplate().getForObject("https://graph.facebook.com/me/friends", ObjectNode.class);
-      ArrayNode data = (ArrayNode) result.get("data");
-      ArrayList<String> friends = new ArrayList<String>();
-      for (JsonNode dataNode : data) {
-        friends.add(dataNode.get("name").getTextValue());
-      }
-      return new ModelAndView("facebook", "friends", friends);
-    }
-    catch (HttpClientErrorException clientError) {
-      if (clientError.getStatusCode().value() == 400) {
-        //there are multiple reasons we could get a 400, but we're going to assume the token was revoked.
-        //we've got a bad token, probably because it's expired or revoked.
-        OAuth2ProtectedResourceDetails resource = getFacebookRestTemplate().getResource();
-        OAuth2SecurityContext context = OAuth2SecurityContextHolder.getContext();
-        if (context != null) {
-          // this one is kind of a hack for this application
-          // the problem is that the facebook friends page doesn't remove the 'code=' request parameter.
-          ((OAuth2SecurityContextImpl) context).setVerificationCode(null);
-        }
-        //clear any stored access tokens...
-        getTokenServices().removeToken(SecurityContextHolder.getContext().getAuthentication(), resource);
-        //go get a new access token...
-        throw new OAuth2AccessTokenRequiredException(resource);
-      }
-      else {
-        throw clientError;
-      }
-    }
-  }
+	@RequestMapping("/info.jsp")
+	public String photos(Model model) throws Exception {
+		try {
+			ObjectNode result = facebookRestTemplate.getForObject("https://graph.facebook.com/me/friends",
+					ObjectNode.class);
+			ArrayNode data = (ArrayNode) result.get("data");
+			ArrayList<String> friends = new ArrayList<String>();
+			for (JsonNode dataNode : data) {
+				friends.add(dataNode.get("name").getTextValue());
+			}
+			model.addAttribute("friends", friends);
+			return "facebook";
+		} catch (HttpClientErrorException clientError) {
+			if (clientError.getStatusCode().value() == 400) {
+				// there are multiple reasons we could get a 400, but we're going to assume the token was revoked.
+				// we've got a bad token, probably because it's expired or revoked.
+				OAuth2ProtectedResourceDetails resource = facebookRestTemplate.getResource();
+				OAuth2SecurityContext context = OAuth2SecurityContextHolder.getContext();
+				if (context != null) {
+					// this one is kind of a hack for this application
+					// the problem is that the facebook friends page doesn't remove the 'code=' request parameter.
+					((OAuth2SecurityContextImpl) context).setVerificationCode(null);
+				}
+				// clear any stored access tokens...
+				tokenServices.removeToken(SecurityContextHolder.getContext().getAuthentication(), resource);
+				// go get a new access token...
+				throw new OAuth2AccessTokenRequiredException(resource);
+			} else {
+				throw clientError;
+			}
+		}
+	}
 
-  public OAuth2RestTemplate getFacebookRestTemplate() {
-    return facebookRestTemplate;
-  }
+	public void setFacebookRestTemplate(OAuth2RestTemplate facebookRestTemplate) {
+		this.facebookRestTemplate = facebookRestTemplate;
+	}
 
-  public void setFacebookRestTemplate(OAuth2RestTemplate facebookRestTemplate) {
-    this.facebookRestTemplate = facebookRestTemplate;
-  }
-
-  public OAuth2ClientTokenServices getTokenServices() {
-    return tokenServices;
-  }
-
-  public void setTokenServices(OAuth2ClientTokenServices tokenServices) {
-    this.tokenServices = tokenServices;
-  }
+	public void setTokenServices(OAuth2ClientTokenServices tokenServices) {
+		this.tokenServices = tokenServices;
+	}
 }
--- tonr2/src/main/java/org/springframework/security/oauth/examples/tonr/mvc/SparklrController.java
+++ tonr2/src/main/java/org/springframework/security/oauth/examples/tonr/mvc/SparklrController.java
@@ -1,28 +1,66 @@
 package org.springframework.security.oauth.examples.tonr.mvc;
 
-import org.springframework.security.oauth.examples.tonr.SparklrService;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.util.Iterator;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.MemoryCacheImageInputStream;
+import javax.servlet.UnavailableException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.oauth.examples.tonr.SparklrService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Ryan Heaton
+ * @author Dave Syer
  */
-public class SparklrController extends AbstractController {
+@Controller
+public class SparklrController {
 
-  private SparklrService sparklrService;
+	private SparklrService sparklrService;
 
-  protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    return new ModelAndView("sparklr", "photoIds", getSparklrService().getSparklrPhotoIds());
-  }
+	@RequestMapping("/photos.jsp")
+	public String photos(Model model) throws Exception {
+		model.addAttribute("photoIds", sparklrService.getSparklrPhotoIds());
+		return "sparklr";
+	}
 
-  public SparklrService getSparklrService() {
-    return sparklrService;
-  }
+	@RequestMapping("/photo/{id}")
+	public ResponseEntity<BufferedImage> photo(@PathVariable String id) throws Exception {
+		InputStream photo = sparklrService.loadSparklrPhoto(id);
+		if (photo == null) {
+			throw new UnavailableException("The requested photo does not exist");
+		}
+		BufferedImage body;
+		MediaType contentType = MediaType.IMAGE_JPEG;
+		Iterator<ImageReader> imageReaders = ImageIO.getImageReadersByMIMEType(contentType.toString());
+		if (imageReaders.hasNext()) {
+			ImageReader imageReader = imageReaders.next();
+			ImageReadParam irp = imageReader.getDefaultReadParam();
+			imageReader.setInput(new MemoryCacheImageInputStream(photo), true);
+			body = imageReader.read(0, irp);
+		}
+		else {
+			throw new HttpMessageNotReadableException(
+					"Could not find javax.imageio.ImageReader for Content-Type [" + contentType + "]");
+		}
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		return new ResponseEntity<BufferedImage>(body, headers, HttpStatus.OK);
+	}
 
-  public void setSparklrService(SparklrService sparklrService) {
-    this.sparklrService = sparklrService;
-  }
+	public void setSparklrService(SparklrService sparklrService) {
+		this.sparklrService = sparklrService;
+	}
 }
