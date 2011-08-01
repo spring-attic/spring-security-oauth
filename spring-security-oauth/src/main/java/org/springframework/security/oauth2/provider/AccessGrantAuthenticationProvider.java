c86f77943664c690f3b1d5bf0a51434b8014ba72
Dave Syer <dsyer@vmware.com>
2011-08-01 17:07:23 +0100

    SECOAUTH-94: fix receiver of web server profile authorization code

--- spring-security-oauth/src/main/java/org/springframework/security/oauth2/provider/AccessGrantAuthenticationProvider.java
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth2/provider/AccessGrantAuthenticationProvider.java
@@ -17,80 +17,80 @@ import java.util.Set;
 
 /**
  * Authentication provider for grants for access to an auth token.
- *  
+ * 
  * @author Ryan Heaton
  */
 public class AccessGrantAuthenticationProvider implements AuthenticationProvider, InitializingBean {
 
-  private ClientDetailsService clientDetailsService;
-  private PasswordEncoder passwordEncoder = new PlaintextPasswordEncoder();
+	private ClientDetailsService clientDetailsService;
+	private PasswordEncoder passwordEncoder = new PlaintextPasswordEncoder();
 
-  public void afterPropertiesSet() throws Exception {
-    Assert.notNull(this.clientDetailsService, "Client details service must be supplied");
-    Assert.notNull(this.passwordEncoder, "Password Encoder must be supplied");
-  }
+	public void afterPropertiesSet() throws Exception {
+		Assert.notNull(this.clientDetailsService, "Client details service must be supplied");
+		Assert.notNull(this.passwordEncoder, "Password Encoder must be supplied");
+	}
 
-  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-    AccessGrantAuthenticationToken clientAuth = (AccessGrantAuthenticationToken) authentication;
-    ClientDetails clientDetails = getClientDetailsService().loadClientByClientId(clientAuth.getClientId());
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		AccessGrantAuthenticationToken clientAuth = (AccessGrantAuthenticationToken) authentication;
+		ClientDetails clientDetails = getClientDetailsService().loadClientByClientId(clientAuth.getClientId());
 
-    if (clientDetails.isSecretRequired()) {
-      String assertedSecret = clientAuth.getClientSecret();
-      if (assertedSecret == null) {
-        throw new InvalidClientException("Client secret is required but not provided.");
-      }
-      else {
-        Object salt = null;
-        if (clientDetails instanceof SaltedClientSecret) {
-          salt = ((SaltedClientSecret) clientDetails).getSalt();
-        }
+		if (clientDetails.isSecretRequired()) {
+			String assertedSecret = clientAuth.getClientSecret();
+			if (assertedSecret == null) {
+				throw new InvalidClientException("Client secret is required but not provided.");
+			} else {
+				Object salt = null;
+				if (clientDetails instanceof SaltedClientSecret) {
+					salt = ((SaltedClientSecret) clientDetails).getSalt();
+				}
 
-        if (!getPasswordEncoder().isPasswordValid(clientDetails.getClientSecret(), assertedSecret, salt)) {
-          throw new InvalidClientException("Invalid client secret.");
-        }
-      }
-    }
+				if (!getPasswordEncoder().isPasswordValid(clientDetails.getClientSecret(), assertedSecret, salt)) {
+					throw new InvalidClientException("Invalid client secret.");
+				}
+			}
+		}
 
-    if (clientDetails.isScoped()) {
-      Set<String> requestedScope = clientAuth.getScope();
-      if (requestedScope.isEmpty()) {
-          throw new InvalidScopeException("Invalid scope (none)");
-        }
-      List<String> validScope = clientDetails.getScope();
-      for (String scope : requestedScope) {
-        if (!validScope.contains(scope)) {
-          throw new InvalidScopeException("Invalid scope: " + scope);
-        }
-      }
-    }
+		if (clientDetails.isScoped()) {
+			Set<String> requestedScope = clientAuth.getScope();
+			if (requestedScope.isEmpty()) {
+				throw new InvalidScopeException("Invalid scope (none)");
+			}
+			List<String> validScope = clientDetails.getScope();
+			for (String scope : requestedScope) {
+				if (!validScope.contains(scope)) {
+					throw new InvalidScopeException("Invalid scope: " + scope);
+				}
+			}
+		}
 
-    List<String> authorizedGrantTypes = clientDetails.getAuthorizedGrantTypes();
-    if (authorizedGrantTypes != null && !authorizedGrantTypes.contains(clientAuth.getGrantType())) {
-      throw new InvalidGrantException("Unauthorized grant type: " + clientAuth.getGrantType());
-    }
+		List<String> authorizedGrantTypes = clientDetails.getAuthorizedGrantTypes();
+		if (authorizedGrantTypes != null && !authorizedGrantTypes.contains(clientAuth.getGrantType())) {
+			throw new InvalidGrantException("Unauthorized grant type: " + clientAuth.getGrantType());
+		}
 
-    return new AuthorizedClientAuthenticationToken(clientAuth.getClientId(), clientAuth.getClientSecret(), clientAuth.getScope(), clientDetails.getAuthorities());
-  }
+		return new AuthorizedClientAuthenticationToken(clientAuth.getClientId(), clientAuth.getClientSecret(),
+				clientAuth.getScope(), clientDetails.getAuthorities());
+	}
 
-  public boolean supports(Class<?> authentication) {
-    return AccessGrantAuthenticationToken.class.isAssignableFrom(authentication);
-  }
+	public boolean supports(Class<?> authentication) {
+		return AccessGrantAuthenticationToken.class.isAssignableFrom(authentication);
+	}
 
-  public ClientDetailsService getClientDetailsService() {
-    return clientDetailsService;
-  }
+	public ClientDetailsService getClientDetailsService() {
+		return clientDetailsService;
+	}
 
-  @Autowired
-  public void setClientDetailsService(ClientDetailsService clientDetailsService) {
-    this.clientDetailsService = clientDetailsService;
-  }
+	@Autowired
+	public void setClientDetailsService(ClientDetailsService clientDetailsService) {
+		this.clientDetailsService = clientDetailsService;
+	}
 
-  public PasswordEncoder getPasswordEncoder() {
-    return passwordEncoder;
-  }
+	public PasswordEncoder getPasswordEncoder() {
+		return passwordEncoder;
+	}
 
-  @Autowired ( required = false )
-  public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
-    this.passwordEncoder = passwordEncoder;
-  }
+	@Autowired(required = false)
+	public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
 }
--- spring-security-oauth/src/main/java/org/springframework/security/oauth2/provider/code/UnconfirmedAuthorizationCodeAuthenticationProvider.java
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth2/provider/code/UnconfirmedAuthorizationCodeAuthenticationProvider.java
@@ -15,73 +15,81 @@ import java.util.Set;
 
 /**
  * Authentication provider that supplies an auth token in exchange for an authorization code.
- *
+ * 
  * @author Ryan Heaton
+ * @author Dave Syer
  */
 public class UnconfirmedAuthorizationCodeAuthenticationProvider implements AuthenticationProvider, InitializingBean {
 
-  private AuthenticationManager authenticationManager;
-  private AuthorizationCodeServices authorizationCodeServices;
+	private AuthenticationManager authenticationManager;
+	private AuthorizationCodeServices authorizationCodeServices;
 
-  public void afterPropertiesSet() throws Exception {
-    Assert.notNull(this.authenticationManager, "An authentication manager must be provided.");
-    Assert.notNull(this.authorizationCodeServices, "Authorization code services must be supplied.");
-  }
+	public void afterPropertiesSet() throws Exception {
+		Assert.notNull(this.authenticationManager, "An authentication manager must be provided.");
+		Assert.notNull(this.authorizationCodeServices, "Authorization code services must be supplied.");
+	}
 
-  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-    AuthorizationCodeAuthenticationToken auth = (AuthorizationCodeAuthenticationToken) authentication;
-    String authorizationCode = auth.getAuthorizationCode();
-    if (authorizationCode == null) {
-      throw new OAuth2Exception("An authorization code must be supplied.");
-    }
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		AuthorizationCodeAuthenticationToken auth = (AuthorizationCodeAuthenticationToken) authentication;
+		String authorizationCode = auth.getAuthorizationCode();
+		if (authorizationCode == null) {
+			throw new OAuth2Exception("An authorization code must be supplied.");
+		}
 
-    OAuth2Authentication<? extends UnconfirmedAuthorizationCodeAuthenticationToken, ? extends Authentication> storedAuth = getAuthorizationCodeServices().consumeAuthorizationCode(authorizationCode);
-    if (storedAuth == null) {
-      throw new InvalidGrantException("Invalid authorization code: " + authorizationCode);
-    }
+		OAuth2Authentication<? extends UnconfirmedAuthorizationCodeAuthenticationToken, ? extends Authentication> storedAuth = getAuthorizationCodeServices()
+				.consumeAuthorizationCode(authorizationCode);
+		if (storedAuth == null) {
+			throw new InvalidGrantException("Invalid authorization code: " + authorizationCode);
+		}
 
-    UnconfirmedAuthorizationCodeAuthenticationToken unconfirmedAuthorizationCodeAuth = storedAuth.getClientAuthentication();
-    if (unconfirmedAuthorizationCodeAuth.getRequestedRedirect() != null && !unconfirmedAuthorizationCodeAuth.getRequestedRedirect().equals(auth.getRequestedRedirect())) {
-      throw new RedirectMismatchException("Redirect URI mismatch.");
-    }
+		UnconfirmedAuthorizationCodeAuthenticationToken unconfirmedAuthorizationCodeAuth = storedAuth
+				.getClientAuthentication();
+		if (unconfirmedAuthorizationCodeAuth.getRequestedRedirect() != null
+				&& !unconfirmedAuthorizationCodeAuth.getRequestedRedirect().equals(auth.getRequestedRedirect())) {
+			throw new RedirectMismatchException("Redirect URI mismatch.");
+		}
 
-    if (auth.getClientId() == null || !auth.getClientId().equals(unconfirmedAuthorizationCodeAuth.getClientId())) {
-      //just a sanity check.
-      throw new InvalidClientException("Client ID mismatch");
-    }
+		if (auth.getClientId() == null || !auth.getClientId().equals(unconfirmedAuthorizationCodeAuth.getClientId())) {
+			// just a sanity check.
+			throw new InvalidClientException("Client ID mismatch");
+		}
 
-    Set<String> authorizationScope = unconfirmedAuthorizationCodeAuth.getScope();
-    Set<String> authScope = auth.getScope();
-    if (!authorizationScope.containsAll(authScope)) {
-      throw new InvalidScopeException("Request for access token scope outside of authorization code scope.");
-    }
+		Set<String> unconfirmedAuthorizationScope = unconfirmedAuthorizationCodeAuth.getScope();
+		Set<String> authorizationScope = auth.getScope();
+		if (!unconfirmedAuthorizationScope.containsAll(authorizationScope)) {
+			throw new InvalidScopeException("Request for access token scope outside of authorization code scope.");
+		}
+		if (authorizationScope.isEmpty()) {
+			authorizationScope = unconfirmedAuthorizationScope;
+		}
 
-    AccessGrantAuthenticationToken confirmedAuth = new AccessGrantAuthenticationToken(auth.getClientId(), auth.getClientSecret(), authScope, "authorization_code");
-    Authentication clientAuth = getAuthenticationManager().authenticate(confirmedAuth);
-    Authentication userAuth = storedAuth.getUserAuthentication();
-    return new OAuth2Authentication<Authentication, Authentication>(clientAuth, userAuth);
-  }
+		AccessGrantAuthenticationToken confirmedAuth = new AccessGrantAuthenticationToken(auth.getClientId(),
+				auth.getClientSecret(), authorizationScope, "authorization_code");
+		Authentication clientAuth = getAuthenticationManager().authenticate(confirmedAuth);
+		Authentication userAuth = storedAuth.getUserAuthentication();
+		return new OAuth2Authentication<Authentication, Authentication>(clientAuth, userAuth);
+	}
 
-  public boolean supports(Class authentication) {
-    return AuthorizationCodeAuthenticationToken.class.isAssignableFrom(authentication);
-  }
+	public boolean supports(Class authentication) {
+		return AuthorizationCodeAuthenticationToken.class.isAssignableFrom(authentication);
+	}
 
-  public AuthenticationManager getAuthenticationManager() {
-    return authenticationManager;
-  }
+	public AuthenticationManager getAuthenticationManager() {
+		return authenticationManager;
+	}
 
-  @Autowired
-  public void setAuthenticationManager(AuthenticationManager authenticationManager) {
-    this.authenticationManager = authenticationManager;
-  }
+	@Autowired
+	public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
 
-  public AuthorizationCodeServices getAuthorizationCodeServices() {
-    return authorizationCodeServices;
-  }
+	public AuthorizationCodeServices getAuthorizationCodeServices() {
+		return authorizationCodeServices;
+	}
 
-  @Autowired
-  public void setAuthorizationCodeServices(AuthorizationCodeServices authorizationCodeServices) {
-    this.authorizationCodeServices = authorizationCodeServices;
-  }
+	@Autowired
+	public void setAuthorizationCodeServices(AuthorizationCodeServices authorizationCodeServices) {
+		this.authorizationCodeServices = authorizationCodeServices;
+	}
 
 }
