445474346d15a30b2acf4d593fcb6d9139c51751
Dave Syer <dsyer@vmware.com>
2011-08-09 10:49:39 +0100

    Factor out some convenience methods and add logging to WebServerProfile

--- spring-security-oauth/src/main/java/org/springframework/security/oauth2/consumer/OAuth2AccessTokenSupport.java
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth2/consumer/OAuth2AccessTokenSupport.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpMethod;
@@ -34,137 +36,158 @@ import org.springframework.web.client.RestTemplate;
  * Base support logic for obtaining access tokens.
  * 
  * @author Ryan Heaton
+ * @author Dave Syer
  */
 public abstract class OAuth2AccessTokenSupport implements InitializingBean {
 
-  public static final MediaType FORM_MEDIA_TYPE = new MediaType("application", "x-www-form-urlencoded");
-  public static final MediaType JSON_MEDIA_TYPE = new MediaType("application", "json");
-  private static final FormHttpMessageConverter FORM_MESSAGE_CONVERTER = new FormHttpMessageConverter();
+	protected final Log logger = LogFactory.getLog(getClass());
 
-  private final RestTemplate restTemplate;
-  private OAuth2SerializationService serializationService = new DefaultOAuth2SerializationService();
-  private ClientAuthenticationHandler authenticationHandler = new DefaultClientAuthenticationHandler();
+	public static final MediaType FORM_MEDIA_TYPE = new MediaType("application", "x-www-form-urlencoded");
+	public static final MediaType JSON_MEDIA_TYPE = new MediaType("application", "json");
+	private static final FormHttpMessageConverter FORM_MESSAGE_CONVERTER = new FormHttpMessageConverter();
 
-  protected OAuth2AccessTokenSupport() {
-    this.restTemplate = new RestTemplate();
-    this.restTemplate.setErrorHandler(new AccessTokenErrorHandler());
-  }
+	private final RestTemplate restTemplate;
+	private OAuth2SerializationService serializationService = new DefaultOAuth2SerializationService();
+	private ClientAuthenticationHandler authenticationHandler = new DefaultClientAuthenticationHandler();
 
-  public void afterPropertiesSet() throws Exception {
-    Assert.notNull(restTemplate, "A RestTemplate is required.");
-    Assert.notNull(serializationService, "OAuth2 serialization service is required.");
-  }
+	protected OAuth2AccessTokenSupport() {
+		this.restTemplate = new RestTemplate();
+		this.restTemplate.setErrorHandler(new AccessTokenErrorHandler());
+	}
 
-  public RestTemplate getRestTemplate() {
-    return restTemplate;
-  }
+	public void afterPropertiesSet() throws Exception {
+		Assert.notNull(restTemplate, "A RestTemplate is required.");
+		Assert.notNull(serializationService, "OAuth2 serialization service is required.");
+	}
 
-  public OAuth2SerializationService getSerializationService() {
-    return serializationService;
-  }
+	public RestTemplate getRestTemplate() {
+		return restTemplate;
+	}
 
-  public void setSerializationService(OAuth2SerializationService serializationService) {
-    this.serializationService = serializationService;
-  }
+	public OAuth2SerializationService getSerializationService() {
+		return serializationService;
+	}
 
-  public ClientAuthenticationHandler getAuthenticationHandler() {
-    return authenticationHandler;
-  }
+	public void setSerializationService(OAuth2SerializationService serializationService) {
+		this.serializationService = serializationService;
+	}
 
-  public void setAuthenticationHandler(ClientAuthenticationHandler authenticationHandler) {
-    this.authenticationHandler = authenticationHandler;
-  }
+	public ClientAuthenticationHandler getAuthenticationHandler() {
+		return authenticationHandler;
+	}
 
-  protected OAuth2AccessToken retrieveToken(MultiValueMap<String, String> form, OAuth2ProtectedResourceDetails resource) {
-    try {
-      return getRestTemplate().execute(resource.getAccessTokenUri(), HttpMethod.POST, new OAuth2AuthTokenCallback(form, resource),
-                                                  new HttpMessageConverterExtractor<OAuth2AccessToken>(OAuth2AccessToken.class, (List) Arrays.asList(new OAuth2AccessTokenMessageConverter())));
-    }
-    catch (OAuth2Exception oe) {
-      throw new OAuth2AccessDeniedException("Access token denied.", resource, oe);
-    }
-    catch (RestClientException rce) {
-      throw new OAuth2AccessDeniedException("Error requesting access token.", resource, rce);
-    }
-  }
+	public void setAuthenticationHandler(ClientAuthenticationHandler authenticationHandler) {
+		this.authenticationHandler = authenticationHandler;
+	}
 
-  /**
-   * Request callback implementation that writes the given object to the request stream.
-   */
-  private class OAuth2AuthTokenCallback implements RequestCallback {
+	protected OAuth2AccessToken retrieveToken(MultiValueMap<String, String> form,
+			OAuth2ProtectedResourceDetails resource) {
 
-    private final MultiValueMap<String, String> form;
-    private final OAuth2ProtectedResourceDetails resource;
+		try {
+			String accessTokenUri = resource.getAccessTokenUri();
 
-    private OAuth2AuthTokenCallback(MultiValueMap<String, String> form, OAuth2ProtectedResourceDetails resource) {
-      this.form = form;
-      this.resource = resource;
-    }
+			if (logger.isDebugEnabled()) {
+				logger.debug("Retrieving token from " + accessTokenUri);
+			}
 
-    public void doWithRequest(ClientHttpRequest request) throws IOException {
-      getAuthenticationHandler().authenticateTokenRequest(this.resource, this.form, request);
-      request.getHeaders().setAccept(Arrays.asList(JSON_MEDIA_TYPE, FORM_MEDIA_TYPE));
-      FORM_MESSAGE_CONVERTER.write(this.form, FORM_MEDIA_TYPE, request);
-    }
-  }
+			return getRestTemplate().execute(
+					accessTokenUri,
+					HttpMethod.POST,
+					new OAuth2AuthTokenCallback(form, resource),
+					new HttpMessageConverterExtractor<OAuth2AccessToken>(OAuth2AccessToken.class, (List) Arrays
+							.asList(new OAuth2AccessTokenMessageConverter())));
 
-  private class AccessTokenErrorHandler extends DefaultResponseErrorHandler {
+		} catch (OAuth2Exception oe) {
 
-    @Override
-    public void handleError(ClientHttpResponse response) throws IOException {
-      MediaType contentType = response.getHeaders().getContentType();
-      if (contentType != null && (response.getStatusCode().value() == 400 || response.getStatusCode().value() == 401)) {
-        if (JSON_MEDIA_TYPE.includes(contentType)) {
-          try {
-            throw getSerializationService().deserializeJsonError(response.getBody());
-          }
-          catch (SerializationException e) {
-            throw new OAuth2Exception("Error getting the access token, and unable to read the details of the error in the JSON response.", e);
-          }
-        }
-        else if (FORM_MEDIA_TYPE.includes(contentType)) {
-          MultiValueMap<String, String> map = FORM_MESSAGE_CONVERTER.read(null, response);
-          throw getSerializationService().deserializeError(map.toSingleValueMap());
-        }
-      }
+			throw new OAuth2AccessDeniedException("Access token denied.", resource, oe);
 
-      super.handleError(response);
-    }
+		} catch (RestClientException rce) {
 
-  }
+			throw new OAuth2AccessDeniedException("Error requesting access token.", resource, rce);
+		}
 
-  private class OAuth2AccessTokenMessageConverter extends AbstractHttpMessageConverter<OAuth2AccessToken> {
+	}
 
-    private OAuth2AccessTokenMessageConverter() {
-      super(new MediaType("*", "*"));
-    }
+	/**
+	 * Request callback implementation that writes the given object to the request stream.
+	 */
+	private class OAuth2AuthTokenCallback implements RequestCallback {
 
-    @Override
-    protected boolean supports(Class<?> clazz) {
-      return OAuth2AccessToken.class.isAssignableFrom(clazz);
-    }
+		private final MultiValueMap<String, String> form;
+		private final OAuth2ProtectedResourceDetails resource;
 
-    @Override
-    protected OAuth2AccessToken readInternal(Class<? extends OAuth2AccessToken> clazz, HttpInputMessage response) throws IOException, HttpMessageNotReadableException {
-      MediaType contentType = response.getHeaders().getContentType();
-      if (contentType != null && JSON_MEDIA_TYPE.includes(contentType)) {
-        try {
-          return getSerializationService().deserializeJsonAccessToken(response.getBody());
-        }
-        catch (SerializationException e) {
-          throw new OAuth2Exception("Error getting the access token, and unable to read the details of the error in the JSON response.", e);
-        }
-      }
-      else {
-        //the spec currently says json is required, but facebook, for example, still returns form-encoded.
-        MultiValueMap<String, String> map = FORM_MESSAGE_CONVERTER.read(null, response);
-        return getSerializationService().deserializeAccessToken(map.toSingleValueMap());
-      }
-    }
+		private OAuth2AuthTokenCallback(MultiValueMap<String, String> form, OAuth2ProtectedResourceDetails resource) {
+			this.form = form;
+			this.resource = resource;
+		}
 
-    @Override
-    protected void writeInternal(OAuth2AccessToken oAuth2AccessToken, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
-      throw new HttpMessageNotWritableException("Access token support shouldn't need to write access tokens.");
-    }
-  }
+		public void doWithRequest(ClientHttpRequest request) throws IOException {
+			getAuthenticationHandler().authenticateTokenRequest(this.resource, this.form, request);
+			request.getHeaders().setAccept(Arrays.asList(JSON_MEDIA_TYPE, FORM_MEDIA_TYPE));
+			FORM_MESSAGE_CONVERTER.write(this.form, FORM_MEDIA_TYPE, request);
+		}
+	}
+
+	private class AccessTokenErrorHandler extends DefaultResponseErrorHandler {
+
+		@Override
+		public void handleError(ClientHttpResponse response) throws IOException {
+			MediaType contentType = response.getHeaders().getContentType();
+			if (contentType != null
+					&& (response.getStatusCode().value() == 400 || response.getStatusCode().value() == 401)) {
+				if (JSON_MEDIA_TYPE.includes(contentType)) {
+					try {
+						throw getSerializationService().deserializeJsonError(response.getBody());
+					} catch (SerializationException e) {
+						throw new OAuth2Exception(
+								"Error getting the access token, and unable to read the details of the error in the JSON response.",
+								e);
+					}
+				} else if (FORM_MEDIA_TYPE.includes(contentType)) {
+					MultiValueMap<String, String> map = FORM_MESSAGE_CONVERTER.read(null, response);
+					throw getSerializationService().deserializeError(map.toSingleValueMap());
+				}
+			}
+
+			super.handleError(response);
+		}
+
+	}
+
+	private class OAuth2AccessTokenMessageConverter extends AbstractHttpMessageConverter<OAuth2AccessToken> {
+
+		private OAuth2AccessTokenMessageConverter() {
+			super(new MediaType("*", "*"));
+		}
+
+		@Override
+		protected boolean supports(Class<?> clazz) {
+			return OAuth2AccessToken.class.isAssignableFrom(clazz);
+		}
+
+		@Override
+		protected OAuth2AccessToken readInternal(Class<? extends OAuth2AccessToken> clazz, HttpInputMessage response)
+				throws IOException, HttpMessageNotReadableException {
+			MediaType contentType = response.getHeaders().getContentType();
+			if (contentType != null && JSON_MEDIA_TYPE.includes(contentType)) {
+				try {
+					return getSerializationService().deserializeJsonAccessToken(response.getBody());
+				} catch (SerializationException e) {
+					throw new OAuth2Exception(
+							"Error getting the access token, and unable to read the details of the error in the JSON response.",
+							e);
+				}
+			} else {
+				// the spec currently says json is required, but facebook, for example, still returns form-encoded.
+				MultiValueMap<String, String> map = FORM_MESSAGE_CONVERTER.read(null, response);
+				return getSerializationService().deserializeAccessToken(map.toSingleValueMap());
+			}
+		}
+
+		@Override
+		protected void writeInternal(OAuth2AccessToken oAuth2AccessToken, HttpOutputMessage outputMessage)
+				throws IOException, HttpMessageNotWritableException {
+			throw new HttpMessageNotWritableException("Access token support shouldn't need to write access tokens.");
+		}
+	}
 }
--- spring-security-oauth/src/main/java/org/springframework/security/oauth2/consumer/webserver/WebServerProfile.java
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth2/consumer/webserver/WebServerProfile.java
@@ -1,106 +1,142 @@
 package org.springframework.security.oauth2.consumer.webserver;
 
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.consumer.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.TreeMap;
 
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.consumer.OAuth2AccessTokenSupport;
+import org.springframework.security.oauth2.consumer.OAuth2Profile;
+import org.springframework.security.oauth2.consumer.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.consumer.OAuth2SecurityContext;
+import org.springframework.security.oauth2.consumer.OAuth2SecurityContextHolder;
+import org.springframework.security.oauth2.consumer.UserRedirectRequiredException;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 /**
  * Implementation of the web server client oauth2 profile.
- *
+ * 
  * @author Ryan Heaton
+ * @author Dave Syer
  */
 public class WebServerProfile extends OAuth2AccessTokenSupport implements OAuth2Profile {
 
-  public OAuth2AccessToken obtainNewAccessToken(OAuth2ProtectedResourceDetails details) throws UserRedirectRequiredException, AccessDeniedException {
-    WebServerProfileResourceDetails resource = (WebServerProfileResourceDetails) details;
-    final OAuth2SecurityContext context = OAuth2SecurityContextHolder.getContext();
-    String authorizationCode = null;
-    if (context != null) {
-      authorizationCode = context.getAuthorizationCode();
-    }
+	public boolean supportsResource(OAuth2ProtectedResourceDetails resource) {
+		return resource instanceof WebServerProfileResourceDetails
+				&& "authorization_code".equals(resource.getGrantType());
+	}
 
-    if (context != null && context.getErrorParameters() != null) {
-      //there was an oauth error...
-      throw getSerializationService().deserializeError(context.getErrorParameters());
-    }
-    else if (authorizationCode == null) {
-      //we don't have an authorization code yet. So first get that.
-      TreeMap<String, String> requestParameters = new TreeMap<String, String>();
-      requestParameters.put("response_type", "code"); //oauth2 spec, section 3
-      requestParameters.put("client_id", resource.getClientId());
-      String redirectUri = resource.getPreEstablishedRedirectUri();
-      if (redirectUri == null) {
-        if (context == null) {
-          throw new IllegalStateException("No OAuth 2 security context has been established: unable to determine the redirect URI for the current context.");
-        }
-        redirectUri = context.getUserAuthorizationRedirectUri();
-        if (redirectUri == null) {
-          throw new IllegalStateException("No redirect URI has been established for the current OAuth 2 security context.");
-        }
-        requestParameters.put("redirect_uri", redirectUri);
-      }
-      else {
-        redirectUri = null;
-      }
+	public OAuth2AccessToken obtainNewAccessToken(OAuth2ProtectedResourceDetails details)
+			throws UserRedirectRequiredException, AccessDeniedException {
 
-      if (resource.isScoped()) {
-        StringBuilder builder = new StringBuilder();
-        List<String> scope = resource.getScope();
-        if (scope != null) {
-          Iterator<String> scopeIt = scope.iterator();
-          while (scopeIt.hasNext()) {
-            builder.append(scopeIt.next());
-            if (scopeIt.hasNext()) {
-              builder.append(' ');
-            }
-          }
-        }
+		WebServerProfileResourceDetails resource = (WebServerProfileResourceDetails) details;
+		OAuth2SecurityContext context = OAuth2SecurityContextHolder.getContext();
 
-        requestParameters.put("scope", builder.toString());
-      }
+		if (context != null && context.getErrorParameters() != null) {
 
-      String stateKey = resource.getState();
-      if (stateKey != null) {
-        requestParameters.put("state", stateKey);
-      }
+			// there was an oauth error...
+			throw getSerializationService().deserializeError(context.getErrorParameters());
 
-      UserRedirectRequiredException redirectException = new UserRedirectRequiredException(resource.getUserAuthorizationUri(), requestParameters);
-      if (redirectUri != null) {
-        redirectException.setStateKey(resource.getState());
-        redirectException.setStateToPreserve(redirectUri);
-      }
-      throw redirectException;
-    }
-    else {
-      MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
-      form.add("grant_type", "authorization_code");
-      form.add("client_id", resource.getClientId());
-      form.add("code", authorizationCode);
+		} else if (context.getAuthorizationCode() == null) {
 
-      Object state = context == null ? null : context.getPreservedState();
-      if (state == null) {
-        //no state preserved? check for a pre-established redirect uri.
-        state = resource.getPreEstablishedRedirectUri();
-      }
+			throw getRedirectForAuthorization(resource, context);
 
-      if (state == null) {
-        //still no redirect uri? just try the one for the current context...
-        state = context == null ? null : context.getUserAuthorizationRedirectUri();
-      }
+		} else {
 
-      form.add("redirect_uri", String.valueOf(state));
+			return retrieveToken(getParametersForTokenRequest(resource, context), resource);
 
-      return retrieveToken(form, resource);
-    }
-  }
+		}
 
-  public boolean supportsResource(OAuth2ProtectedResourceDetails resource) {
-    return resource instanceof WebServerProfileResourceDetails && "authorization_code".equals(resource.getGrantType());
-  }
+	}
+
+	private MultiValueMap<String, String> getParametersForTokenRequest(WebServerProfileResourceDetails resource,
+			OAuth2SecurityContext context) {
+
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
+		form.add("grant_type", "authorization_code");
+		form.add("client_id", resource.getClientId());
+		form.add("code", context.getAuthorizationCode());
+
+		Object state = context == null ? null : context.getPreservedState();
+		if (state == null) {
+			// no state preserved? check for a pre-established redirect uri.
+			state = resource.getPreEstablishedRedirectUri();
+		}
+
+		if (state == null) {
+			// still no redirect uri? just try the one for the current context...
+			state = context == null ? null : context.getUserAuthorizationRedirectUri();
+		}
+
+		form.add("redirect_uri", String.valueOf(state));
+
+		return form;
+
+	}
+
+	private UserRedirectRequiredException getRedirectForAuthorization(WebServerProfileResourceDetails resource,
+			OAuth2SecurityContext context) {
+
+		// we don't have an authorization code yet. So first get that.
+		TreeMap<String, String> requestParameters = new TreeMap<String, String>();
+		requestParameters.put("response_type", "code"); // oauth2 spec, section 3
+		requestParameters.put("client_id", resource.getClientId());
+
+		String redirectUri = resource.getPreEstablishedRedirectUri();
+		if (redirectUri == null) {
+
+			if (context == null) {
+				throw new IllegalStateException(
+						"No OAuth 2 security context has been established: unable to determine the redirect URI for the current context.");
+			}
+			redirectUri = context.getUserAuthorizationRedirectUri();
+			if (redirectUri == null) {
+				throw new IllegalStateException(
+						"No redirect URI has been established for the current OAuth 2 security context.");
+			}
+			requestParameters.put("redirect_uri", redirectUri);
+
+		} else {
+
+			redirectUri = null;
+
+		}
+
+		if (resource.isScoped()) {
+
+			StringBuilder builder = new StringBuilder();
+			List<String> scope = resource.getScope();
+
+			if (scope != null) {
+				Iterator<String> scopeIt = scope.iterator();
+				while (scopeIt.hasNext()) {
+					builder.append(scopeIt.next());
+					if (scopeIt.hasNext()) {
+						builder.append(' ');
+					}
+				}
+			}
+
+			requestParameters.put("scope", builder.toString());
+		}
+
+		String stateKey = resource.getState();
+		if (stateKey != null) {
+			requestParameters.put("state", stateKey);
+		}
+
+		UserRedirectRequiredException redirectException = new UserRedirectRequiredException(
+				resource.getUserAuthorizationUri(), requestParameters);
+
+		if (redirectUri != null) {
+			redirectException.setStateKey(resource.getState());
+			redirectException.setStateToPreserve(redirectUri);
+		}
+
+		return redirectException;
+
+	}
+
 }
