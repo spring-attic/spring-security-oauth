a7812e8fd15baee0f33cdaf44e2341b1ecb04df7
Dave Syer <dsyer@vmware.com>
2011-10-17 11:52:36 +0100

    SECOAUTH-133: Add Serializable to RefreshTokenDetails

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/refresh/RefreshTokenDetails.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/refresh/RefreshTokenDetails.java
@@ -1,13 +1,15 @@
 package org.springframework.security.oauth2.provider.refresh;
 
+import java.io.Serializable;
 import java.util.Set;
 
 /**
  * Holder for refresh token details.
- *
+ * 
  * @author Ryan Heaton
+ * @author Dave Syer
  */
-public class RefreshTokenDetails {
+public class RefreshTokenDetails implements Serializable {
 
 	private final String refreshToken;
 	private final Set<String> scope;
