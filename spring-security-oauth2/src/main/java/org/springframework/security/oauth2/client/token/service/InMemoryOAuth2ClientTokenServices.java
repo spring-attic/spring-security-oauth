57cdd661108d8714d6207a01cb984875f388956b
Dave Syer <dsyer@vmware.com>
2011-11-15 11:27:40 +0000

    SECOAUTH-159: store tokens by resource if no authentication

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/client/token/service/InMemoryOAuth2ClientTokenServices.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/client/token/service/InMemoryOAuth2ClientTokenServices.java
@@ -1,51 +1,60 @@
 package org.springframework.security.oauth2.client.token.service;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+
 /**
  * Token services that simply stores any tokens in memory.
- *
+ * 
  * @author Ryan Heaton
+ * @author Dave Syer
  */
 public class InMemoryOAuth2ClientTokenServices implements OAuth2ClientTokenServices {
 
-  private static final Map<String, Map<String, OAuth2AccessToken>> USER_TO_RESOURCE_TO_TOKEN = new ConcurrentHashMap<String, Map<String, OAuth2AccessToken>>();
+	private static final Map<String, Map<String, OAuth2AccessToken>> USER_TO_RESOURCE_TO_TOKEN = new ConcurrentHashMap<String, Map<String, OAuth2AccessToken>>();
 
-  public OAuth2AccessToken getToken(Authentication authentication, OAuth2ProtectedResourceDetails resource) {
-    if (authentication != null && authentication.isAuthenticated()) {
-      Map<String, OAuth2AccessToken> resourceMap = USER_TO_RESOURCE_TO_TOKEN.get(authentication.getName());
-      return resourceMap == null ? null : resourceMap.get(resource.getId());
-    }
+	private static final Map<OAuth2ProtectedResourceDetails, OAuth2AccessToken> RESOURCE_TO_TOKEN = new ConcurrentHashMap<OAuth2ProtectedResourceDetails, OAuth2AccessToken>();
 
-    return null;
-  }
+	public OAuth2AccessToken getToken(Authentication authentication, OAuth2ProtectedResourceDetails resource) {
+		if (authentication != null && authentication.isAuthenticated()) {
+			Map<String, OAuth2AccessToken> resourceMap = USER_TO_RESOURCE_TO_TOKEN.get(authentication.getName());
+			return resourceMap == null ? null : resourceMap.get(resource.getId());
+		}
 
-  public void storeToken(Authentication authentication, OAuth2ProtectedResourceDetails resource, OAuth2AccessToken token) {
-    if (authentication != null && authentication.isAuthenticated()) {
-      Map<String, OAuth2AccessToken> resourceMap = USER_TO_RESOURCE_TO_TOKEN.get(authentication.getName());
-      if (resourceMap == null) {
-        resourceMap = new ConcurrentHashMap<String, OAuth2AccessToken>();
-        USER_TO_RESOURCE_TO_TOKEN.put(authentication.getName(), resourceMap);
-      }
-      resourceMap.put(resource.getId(), token);
-    }
-  }
+		return RESOURCE_TO_TOKEN.get(resource);
+	}
 
-  public void updateToken(Authentication authentication, OAuth2ProtectedResourceDetails resource, OAuth2AccessToken oldToken, OAuth2AccessToken replacement) {
-    storeToken(authentication, resource, replacement);
-  }
+	public void storeToken(Authentication authentication, OAuth2ProtectedResourceDetails resource,
+			OAuth2AccessToken token) {
+		if (authentication != null && authentication.isAuthenticated()) {
+			Map<String, OAuth2AccessToken> resourceMap = USER_TO_RESOURCE_TO_TOKEN.get(authentication.getName());
+			if (resourceMap == null) {
+				resourceMap = new ConcurrentHashMap<String, OAuth2AccessToken>();
+				USER_TO_RESOURCE_TO_TOKEN.put(authentication.getName(), resourceMap);
+			}
+			resourceMap.put(resource.getId(), token);
+		} else {
+			RESOURCE_TO_TOKEN.put(resource, token);
+		}
+	}
 
-  public void removeToken(Authentication authentication, OAuth2ProtectedResourceDetails resource) {
-    if (authentication != null && authentication.isAuthenticated()) {
-      Map<String, OAuth2AccessToken> resourceMap = USER_TO_RESOURCE_TO_TOKEN.get(authentication.getName());
-      if (resourceMap != null) {
-        resourceMap.remove(resource.getId());
-      }
-    }
-  }
+	public void updateToken(Authentication authentication, OAuth2ProtectedResourceDetails resource,
+			OAuth2AccessToken oldToken, OAuth2AccessToken replacement) {
+		storeToken(authentication, resource, replacement);
+	}
+
+	public void removeToken(Authentication authentication, OAuth2ProtectedResourceDetails resource) {
+		if (authentication != null && authentication.isAuthenticated()) {
+			Map<String, OAuth2AccessToken> resourceMap = USER_TO_RESOURCE_TO_TOKEN.get(authentication.getName());
+			if (resourceMap != null) {
+				resourceMap.remove(resource.getId());
+			}
+		} else {
+			RESOURCE_TO_TOKEN.remove(resource);
+		}
+	}
 }
---
+++ spring-security-oauth2/src/test/java/org/springframework/security/oauth2/client/token/service/TestInMemoryOAuth2ClientTokenServices.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2006-2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.springframework.security.oauth2.client.token.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.resource.BaseOAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+
+/**
+ * @author Dave Syer
+ * 
+ */
+public class TestInMemoryOAuth2ClientTokenServices {
+
+	private InMemoryOAuth2ClientTokenServices services = new InMemoryOAuth2ClientTokenServices();
+	private BaseOAuth2ProtectedResourceDetails resource;
+	
+	public TestInMemoryOAuth2ClientTokenServices() {
+		resource = new BaseOAuth2ProtectedResourceDetails();
+		resource.setId("resource");
+	}
+
+	@Test
+	public void testNonNullAuthentication() {
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("foo", "bar",
+				Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+		services.storeToken(authentication, resource, new OAuth2AccessToken());
+		OAuth2AccessToken token = services.getToken(authentication, resource);
+		assertNotNull(token);
+	}
+
+	@Test
+	public void testNullAuthentication() {
+		services.storeToken(null, resource, new OAuth2AccessToken());
+		OAuth2AccessToken token = services.getToken(null, resource);
+		assertNotNull(token);
+	}
+
+}
