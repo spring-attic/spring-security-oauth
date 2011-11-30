31a23ae17824644c33c884f6ba17c39044405f2e
Krishna Sundaresan <calvinkrishy@gmail.com>
2011-11-30 15:41:55 -0500

    Minimal test case for AuthorizationBeanDefinitionParser

---
+++ spring-security-oauth2/src/test/java/org/springframework/security/oauth2/config/TestAuthorizationServerBeanDefinitionParser.java
@@ -0,0 +1,15 @@
+package org.springframework.security.oauth2.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration
+@RunWith (SpringJUnit4ClassRunner.class)
+public class TestAuthorizationServerBeanDefinitionParser {
+
+	@Test
+	public void testDefaultTokenServices(){
+	}
+}
