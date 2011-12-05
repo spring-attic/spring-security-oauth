6988bd112f3f29907a02a769e089cb25cb907a10
Krishna Sundaresan <calvinkrishy@gmail.com>
2011-12-05 17:33:37 -0500

    Use OAuth2Parameters

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/JerseyTokenEndPoint.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/JerseyTokenEndPoint.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2SerializationService;
 import org.springframework.security.oauth2.common.exceptions.UnsupportedGrantTypeException;
 import org.springframework.security.oauth2.common.util.OAuth2Utils;
+import org.springframework.security.oauth2.provider.endpoint.params.OAuth2Parameters;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
@@ -17,40 +18,24 @@ import java.util.Set;
 @Path("/oauth/token")
 public class JerseyTokenEndPoint extends JerseyAbstractEndPoint {
 
-	private String defaultGrantType = "authorization_code";
-
 	private final OAuth2SerializationService serializationService = new DefaultOAuth2SerializationService();
 
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAccessToken(@FormParam("grant_type") String grantType,
-								   @FormParam("client_secret") String clientSecretParam,
-								   @FormParam("client_id") String clientIdParam,
-								   @FormParam("scope") String scopeParam,
-							   	   @FormParam("username") String userName,
-								   @FormParam("password") String password,
+	public Response getAccessToken(@Context OAuth2Parameters oAuth2Parameters,
 								   @Context HttpHeaders headers) {
 
-		if(null == grantType){
-			grantType = defaultGrantType;
-		}
-
-		Map<String, String> parameters = new HashMap<String, String>();
-		parameters.put("client_secret", clientSecretParam);
-		parameters.put("client_id", clientIdParam);
-		parameters.put("scope", scopeParam);
-		parameters.put("username", userName);
-		parameters.put("password", password);
+		Map<String, String> parameters = oAuth2Parameters.getParameters();
 
 		String[] clientValues = findClientSecret(headers, parameters);
 		String clientId = clientValues[0];
 		String clientSecret = clientValues[1];
-		Set<String> scope = OAuth2Utils.parseScope(scopeParam);
+		Set<String> scope = OAuth2Utils.parseScope(oAuth2Parameters.getScope());
 
-		OAuth2AccessToken token = getTokenGranter().grant(grantType, parameters, clientId, clientSecret, scope);
+		OAuth2AccessToken token = getTokenGranter().grant(oAuth2Parameters.getGrantType(), parameters, clientId, clientSecret, scope);
 		if (token == null) {
-			throw new UnsupportedGrantTypeException("Unsupported grant type: " + grantType);
+			throw new UnsupportedGrantTypeException("Unsupported grant type: " + oAuth2Parameters.getGrantType());
 		}
 
 		return getResponse(token);
@@ -63,17 +48,12 @@ public class JerseyTokenEndPoint extends JerseyAbstractEndPoint {
 
 		CacheControl cacheControl = new CacheControl();
 		cacheControl.setNoStore(true);
-		Response response = Response.ok()
-				                    .cacheControl(cacheControl)
-									.type(MediaType.APPLICATION_JSON)
-								    .entity(serializedToken)
-				                    .build();
-		return response;
+		return Response.ok()
+					   .cacheControl(cacheControl)
+					   .type(MediaType.APPLICATION_JSON)
+					   .entity(serializedToken)
+					   .build();
 
-	}
-
-	public void setDefaultGrantType(String defaultGrantType) {
-		this.defaultGrantType = defaultGrantType;
 	}
 
 }
