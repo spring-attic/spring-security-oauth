d8ac98c84159e2ee0d1a68960a59f20c8600a4a5
Jilles van Gurp <incoming@jillesvangurp.com>
2012-01-08 12:52:10 +0100

    SECOAUTH-180: Rewrite InMemoryNonceServices to use less memory and cleanup more efficiently. Expand unit tests to cover more edge cases.

--- spring-security-oauth/src/test/java/org/springframework/security/oauth/provider/nonce/TestInMemoryNonceServices.java
+++ spring-security-oauth/src/test/java/org/springframework/security/oauth/provider/nonce/TestInMemoryNonceServices.java
@@ -1,66 +1,79 @@
 package org.springframework.security.oauth.provider.nonce;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.UUID;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.oauth.provider.BaseConsumerDetails;
+import org.springframework.security.oauth.provider.ConsumerDetails;
 
 /**
  * @author Ryan Heaton
+ * @author Jilles van Gurp
  */
 public class TestInMemoryNonceServices {
 
-	// seconds since epoch
-	final long timestampForFirstRequest = System.currentTimeMillis() / 1000;
+	private long now;
+	private final InMemoryNonceServices nonceServices = new InMemoryNonceServices();
 
 	@Before
 	public void setUp() throws Exception {
-		InMemoryNonceServices.TIMESTAMP_ENTRIES.clear();
+		// seconds since epoch, reset for every test
+		now = System.currentTimeMillis() / 1000;
+		nonceServices.setValidityWindowSeconds(10);
+		InMemoryNonceServices.NONCES.clear();
 	}
 
 	@Test
-	public void testValidateNonceSecondRequestWithNewTimestampJustBefore() {
-		final InMemoryNonceServices nonceServices = new InMemoryNonceServices();
-		final BaseConsumerDetails consumerDetails = new BaseConsumerDetails();
-		consumerDetails.setConsumerKey("foo");
-		String nonce = UUID.randomUUID().toString();
-		nonceServices.validateNonce(consumerDetails, this.timestampForFirstRequest - 15, nonce);
-		nonce = UUID.randomUUID().toString();
-		nonceServices.validateNonce(consumerDetails, this.timestampForFirstRequest - 20, nonce);
-		// The list of entires should only contain two timestamps.
-		assertEquals(2, InMemoryNonceServices.TIMESTAMP_ENTRIES.get("foo").size());
+	public void shouldAcceptSameNonceWithDifferentTimestamp() {
+		String nonce = nonce();
+		nonceServices.validateNonce(consumer("foo"), now, nonce);
+		nonceServices.validateNonce(consumer("foo"), now+5, nonce);
+		assertEquals(2, InMemoryNonceServices.NONCES.size());
+	}
+
+	@Test(expected=NonceAlreadyUsedException.class)
+	public void shouldRejectAlreadyUsedNonceWithSameTimestamp() {
+		String nonce = nonce();
+		nonceServices.validateNonce(consumer("foo"), now, nonce);
+		nonceServices.validateNonce(consumer("foo"), now, nonce);
+	}
+
+	@Test(expected=CredentialsExpiredException.class)
+	public void shouldRejectTooOldTimestamp() {
+		nonceServices.validateNonce(consumer("foo"), now-11, nonce());
 	}
 
 	@Test
-	public void testValidateNonceSecondRequestWithSameTimestamp() {
-		final InMemoryNonceServices nonceServices = new InMemoryNonceServices();
-		final BaseConsumerDetails consumerDetails = new BaseConsumerDetails();
-		consumerDetails.setConsumerKey("foo");
-		String nonce = UUID.randomUUID().toString();
-		nonceServices.validateNonce(consumerDetails, this.timestampForFirstRequest + 10, nonce);
-		nonce = UUID.randomUUID().toString();
-		nonceServices.validateNonce(consumerDetails, this.timestampForFirstRequest + 10, nonce);
-		// The list of entires should only contain one timestamp.
-		assertEquals(1, InMemoryNonceServices.TIMESTAMP_ENTRIES.get("foo").size());
+	public void shouldAcceptSameNonceFromDifferentConsumers() {
+		String nonce = nonce();
+		nonceServices.validateNonce(consumer("foo"), now, nonce);
+		nonceServices.validateNonce(consumer("bar"), now, nonce);
+		assertEquals(2, InMemoryNonceServices.NONCES.size());
 	}
 
 	@Test
-	public void testValidateNonceSecondRequestWithNewTimestampJustAfter() {
-		final InMemoryNonceServices nonceServices = new InMemoryNonceServices();
-		final BaseConsumerDetails consumerDetails = new BaseConsumerDetails();
-		consumerDetails.setConsumerKey("foo");
-		String nonce = UUID.randomUUID().toString();
-		nonceServices.validateNonce(consumerDetails, this.timestampForFirstRequest, nonce);
-		nonce = UUID.randomUUID().toString();
-		nonceServices.validateNonce(consumerDetails, this.timestampForFirstRequest + 5, nonce);
-		// A ConcurrentModificationException should not be thrown.
-		assertTrue(true);
-		// The list of entires should only contain two timestamps.
-		assertEquals(2, InMemoryNonceServices.TIMESTAMP_ENTRIES.get("foo").size());
+	public void shouldRemoveOldNonces() {
+		// order should not matter
+		InMemoryNonceServices.NONCES.add(new InMemoryNonceServices.NonceEntry("foo", now-2, nonce()));
+		InMemoryNonceServices.NONCES.add(new InMemoryNonceServices.NonceEntry("foo", now-11, nonce()));
+		InMemoryNonceServices.NONCES.add(new InMemoryNonceServices.NonceEntry("foo", now+8, nonce()));
+		InMemoryNonceServices.NONCES.add(new InMemoryNonceServices.NonceEntry("foo", now-15, nonce()));
+		assertEquals(4, InMemoryNonceServices.NONCES.size());
+		nonceServices.validateNonce(consumer("foo"), now, nonce());
+		assertEquals("should have removed two from the original four nonces and added one",3, InMemoryNonceServices.NONCES.size());
 	}
 
+	private String nonce() {
+		return UUID.randomUUID().toString();
+	}
+
+	private ConsumerDetails consumer(String name) {
+		BaseConsumerDetails details = new BaseConsumerDetails();
+		details.setConsumerKey(name);
+		return details;
+	}
 }
