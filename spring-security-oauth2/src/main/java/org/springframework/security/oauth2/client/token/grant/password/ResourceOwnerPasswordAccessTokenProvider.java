fee0d9b07f86a5387a302d21f796ab88df516511
Dave Syer <dsyer@vmware.com>
2012-03-25 14:51:43 +0100

    SECOAUTH-216: add integration tests for refresh token client support, and ensure refresh works with password grants

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/client/token/grant/password/ResourceOwnerPasswordAccessTokenProvider.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/client/token/grant/password/ResourceOwnerPasswordAccessTokenProvider.java
@@ -26,12 +26,15 @@ public class ResourceOwnerPasswordAccessTokenProvider extends OAuth2AccessTokenS
 	}
 
 	public boolean supportsRefresh(OAuth2ProtectedResourceDetails resource) {
-		return false;
+		return supportsResource(resource);
 	}
 
 	public OAuth2AccessToken refreshAccessToken(OAuth2ProtectedResourceDetails resource,
 			OAuth2RefreshToken refreshToken, AccessTokenRequest request) throws UserRedirectRequiredException {
-		return null;
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
+		form.add("grant_type", "refresh_token");
+		form.add("refresh_token", refreshToken.getValue());
+		return retrieveToken(form, resource);
 	}
 
 	public OAuth2AccessToken obtainAccessToken(OAuth2ProtectedResourceDetails details, AccessTokenRequest request)
