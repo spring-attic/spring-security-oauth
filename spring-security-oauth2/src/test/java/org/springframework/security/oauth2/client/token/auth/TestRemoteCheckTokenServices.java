1be6ca0b0c9d4959650858adea4ad52f3faf928e
Vidya Valmikinathan <vidya@rbcon.com>
2012-09-04 17:05:30 -0700

    Remove wrong license notice from TestRemoteCheckTokenServices.

--- spring-security-oauth2/src/test/java/org/springframework/security/oauth2/client/token/auth/TestRemoteCheckTokenServices.java
+++ spring-security-oauth2/src/test/java/org/springframework/security/oauth2/client/token/auth/TestRemoteCheckTokenServices.java
@@ -1,15 +1,4 @@
-package org.springframework.security.oauth2.client.token.auth;/*
- * Cloud Foundry 2012.02.03 Beta
- * Copyright (c) [2009-2012] VMware, Inc. All Rights Reserved.
- *
- * This product is licensed to you under the Apache License, Version 2.0 (the "License").
- * You may not use this product except in compliance with the License.
- *
- * This product includes a number of subcomponents with
- * separate copyright notices and license terms. Your use of these
- * subcomponents is subject to the terms and conditions of the
- * subcomponent's license, as noted in the LICENSE file.
- */
+package org.springframework.security.oauth2.client.token.auth;
 
 import junit.framework.TestCase;
 import org.junit.Test;
