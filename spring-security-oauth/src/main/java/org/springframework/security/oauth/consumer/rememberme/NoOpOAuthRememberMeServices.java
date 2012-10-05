430f20ad6847e11121e8dba966a825b5bdb6c6f4
Dave Syer <dsyer@vmware.com>
2012-10-05 13:38:52 +0100

    SECOAUTH-142: add javadocs to NoOpOAuthRememberMeServices

--- spring-security-oauth/src/main/java/org/springframework/security/oauth/consumer/rememberme/NoOpOAuthRememberMeServices.java
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth/consumer/rememberme/NoOpOAuthRememberMeServices.java
@@ -7,17 +7,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
- * Basic, no-op implementation of the remember-me services.
+ * Basic, no-op implementation of the remember-me services. Not very useful in a 3-legged OAuth flow, but for a 2-legged
+ * system where there are no request tokens to store in between requests it keeps the consumer stateless at the price of
+ * obtaining a new access token for every request.
  * 
  * @author Ryan Heaton
  */
 public class NoOpOAuthRememberMeServices implements OAuthRememberMeServices {
 
-  public Map<String, OAuthConsumerToken> loadRememberedTokens(HttpServletRequest request, HttpServletResponse response) {
-    return null;
-  }
+	public Map<String, OAuthConsumerToken> loadRememberedTokens(HttpServletRequest request, HttpServletResponse response) {
+		return null;
+	}
 
-  public void rememberTokens(Map<String, OAuthConsumerToken> tokens, HttpServletRequest request, HttpServletResponse response) {
-  }
+	public void rememberTokens(Map<String, OAuthConsumerToken> tokens, HttpServletRequest request,
+			HttpServletResponse response) {
+	}
 
 }
