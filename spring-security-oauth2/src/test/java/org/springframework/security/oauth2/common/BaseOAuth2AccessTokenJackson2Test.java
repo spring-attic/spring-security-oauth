09feff7d8ff6b5a2f77c40ad196b55bf162806d6
Brian Clozel <brian.clozel@gmail.com>
2012-11-23 11:59:43 +0100

    add specific jackson2 tests (with their own jackson2 objectMapper instance)

---
+++ spring-security-oauth2/src/test/java/org/springframework/security/oauth2/common/BaseOAuth2AccessTokenJackson2Test.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2006-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.springframework.security.oauth2.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.*;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+/**
+ * Base class for testing Jackson serialization and deserialization of {@link org.springframework.security.oauth2.common.OAuth2AccessToken}.
+ *
+ * @author Rob Winch
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ System.class })
+abstract class BaseOAuth2AccessTokenJackson2Test {
+	protected static final String ACCESS_TOKEN_EMPTYSCOPE = "{\"access_token\":\"token-value\",\"token_type\":\"bearer\",\"refresh_token\":\"refresh-value\",\"expires_in\":10,\"scope\":\"\"}";
+
+	protected static final String ACCESS_TOKEN_MULTISCOPE = "{\"access_token\":\"token-value\",\"token_type\":\"bearer\",\"refresh_token\":\"refresh-value\",\"expires_in\":10,\"scope\":\"read write\"}";
+
+	protected static final String ACCESS_TOKEN_NOSCOPE = "{\"access_token\":\"token-value\",\"token_type\":\"bearer\",\"refresh_token\":\"refresh-value\",\"expires_in\":10}";
+
+	protected static final String ACCESS_TOKEN_NOREFRESH = "{\"access_token\":\"token-value\",\"token_type\":\"bearer\",\"expires_in\":10}";
+
+	protected static final String ACCESS_TOKEN_SINGLESCOPE = "{\"access_token\":\"token-value\",\"token_type\":\"bearer\",\"refresh_token\":\"refresh-value\",\"expires_in\":10,\"scope\":\"write\"}";
+
+	protected static final String ACCESS_TOKEN_ADDITIONAL_INFO = "{\"access_token\":\"token-value\",\"token_type\":\"bearer\",\"one\":\"two\",\"three\":4,\"five\":{\"six\":7}}";
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Mock
+	protected Date expiration;
+
+	protected DefaultOAuth2AccessToken accessToken;
+
+	protected ObjectMapper mapper;
+
+	protected Map<String, Object> additionalInformation;
+
+	public BaseOAuth2AccessTokenJackson2Test() {
+		super();
+	}
+
+	@Before
+	public void setUp() {
+		mockStatic(System.class);
+		long now = 1323123715041L;
+		when(System.currentTimeMillis()).thenReturn(now);
+		when(expiration.before(any(Date.class))).thenReturn(false);
+		when(expiration.getTime()).thenReturn(now + 10000);
+
+		accessToken = new DefaultOAuth2AccessToken("token-value");
+		accessToken.setExpiration(expiration);
+		mapper = new ObjectMapper();
+		DefaultOAuth2RefreshToken refreshToken = new DefaultOAuth2RefreshToken("refresh-value");
+		accessToken.setRefreshToken(refreshToken);
+		Set<String> scope = new TreeSet<String>();
+		scope.add("read");
+		scope.add("write");
+		accessToken.setScope(scope);
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		map.put("one", "two");
+		map.put("three", 4);
+		map.put("five", Collections.singletonMap("six", 7));
+		additionalInformation = map;
+	}
+}
