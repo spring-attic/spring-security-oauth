19ed2323467bcea6d89366de0075d256af93b8f5
Alex Rau <alex@leansync.com>
2012-12-24 18:36:16 +0100

    SECOAUTH-142: make session peristed tokens optional

--- spring-security-oauth/src/main/java/org/springframework/security/oauth/consumer/rememberme/HttpSessionOAuthRememberMeServices.java
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth/consumer/rememberme/HttpSessionOAuthRememberMeServices.java
@@ -1,34 +1,57 @@
 package org.springframework.security.oauth.consumer.rememberme;
 
-import org.springframework.security.oauth.consumer.OAuthConsumerToken;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Map;
+
+import org.springframework.security.oauth.consumer.OAuthConsumerToken;
 
 /**
- * Default implementation of the OAuth2 rememberme services. Just stores everything in the session.
+ * Default implementation of the OAuth2 rememberme services. Just stores everything in the session by default. Storing
+ * access token can be suppressed to reduce long-term expose of these tokens in the underlying HTTP session.
  * 
  * @author Ryan Heaton
+ * @author Alex Rau
  */
 public class HttpSessionOAuthRememberMeServices implements OAuthRememberMeServices {
 
-  public static final String REMEMBERED_TOKENS_KEY = HttpSessionOAuthRememberMeServices.class.getName() + "#REMEMBERED_TOKENS";
+	public static final String REMEMBERED_TOKENS_KEY = HttpSessionOAuthRememberMeServices.class.getName()
+			+ "#REMEMBERED_TOKENS";
 
-  public Map<String, OAuthConsumerToken> loadRememberedTokens(HttpServletRequest request, HttpServletResponse response) {
-    HttpSession session = request.getSession(false);
-    Map<String, OAuthConsumerToken> rememberedTokens = null;
-    if (session != null) {
-      rememberedTokens = (Map<String, OAuthConsumerToken>) session.getAttribute(REMEMBERED_TOKENS_KEY);
-    }
-    return rememberedTokens;
-  }
+	private boolean storeAccessTokens = true;
 
-  public void rememberTokens(Map<String, OAuthConsumerToken> tokens, HttpServletRequest request, HttpServletResponse response) {
-    HttpSession session = request.getSession(false);
-    if (session != null) {
-      session.setAttribute(REMEMBERED_TOKENS_KEY, tokens);
-    }
-  }
+	@SuppressWarnings("unchecked")
+	public Map<String, OAuthConsumerToken> loadRememberedTokens(HttpServletRequest request, HttpServletResponse response) {
+		
+		HttpSession session = request.getSession(false);
+
+		if (session != null) {
+			return (Map<String, OAuthConsumerToken>) session.getAttribute(REMEMBERED_TOKENS_KEY);
+		}
+		
+		return null;
+	}
+
+	public void rememberTokens(Map<String, OAuthConsumerToken> tokens, HttpServletRequest request,
+			HttpServletResponse response) {
+
+		HttpSession session = request.getSession(false);
+
+		if (session == null) {
+			return;
+		}
+
+		Map<String, OAuthConsumerToken> requestTokensOnly = new HashMap<String, OAuthConsumerToken>();
+
+		for (Map.Entry<String, OAuthConsumerToken> token : tokens.entrySet()) {
+			if (storeAccessTokens && !token.getValue().isAccessToken())
+				requestTokensOnly.put(token.getKey(), token.getValue());
+
+		}
+
+		session.setAttribute(REMEMBERED_TOKENS_KEY, requestTokensOnly);
+	}
 }
