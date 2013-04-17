7e91786e9f03fdee680e08c5fd757ed86963262a
Amanda Anganes <aanganes@mitre.org>
2013-04-17 09:51:12 -0400

    removed unused imports

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/AuthorizationRequest.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/AuthorizationRequest.java
@@ -2,7 +2,6 @@ package org.springframework.security.oauth2.provider;
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
