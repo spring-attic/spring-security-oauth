c4dbabb20fcf6cf6b1feadaf443a08112091282f
Amanda Anganes <aanganes@mitre.org>
2013-08-15 10:40:17 -0400

    AuthorizationRequest.createOAuth2Request mistakenly populated the requestParameters map with the approvalParameters map. Fixed method to use correct map.

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/AuthorizationRequest.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/AuthorizationRequest.java
@@ -103,7 +103,7 @@ public class AuthorizationRequest extends BaseRequest {
 	}
 
 	public OAuth2Request createOAuth2Request() {
-		return new OAuth2Request(getApprovalParameters(), getClientId(), getAuthorities(), isApproved(), getScope(), getResourceIds(), getRedirectUri(), getExtensions());
+		return new OAuth2Request(getRequestParameters(), getClientId(), getAuthorities(), isApproved(), getScope(), getResourceIds(), getRedirectUri(), getExtensions());
 	}
 
 	/**
