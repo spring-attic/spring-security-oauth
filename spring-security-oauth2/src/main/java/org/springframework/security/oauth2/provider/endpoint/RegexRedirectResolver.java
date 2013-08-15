ff8cb0cb84bd6bc504384915182b03d818b48acd
OhadR <ohadr@watchdox.com>
2013-08-15 21:36:25 +0300

    Create RegexRedirectResolver.java
    there are cases where the redirect-uri consists of a subdomain, and 
    each customer has its own subdomain, hence a different redirect-uri. So we need a resolver that will validate the URL not as the default impl (that checks startsWith() ), but something that will check only the suffix 
     * of the context path.

---
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/RegexRedirectResolver.java
@@ -0,0 +1,30 @@
+package com.watchdox.butke.endpoint;
+
+import org.springframework.security.oauth2.provider.endpoint.DefaultRedirectResolver;
+
+/**
+ * there are cases where the redirect-uri consists of a subdomain, and 
+ * each customer has its own subdomain, hence a different redirect-uri. So we need a resolver that will validate
+ * the URL not as the default impl (that checks startsWith() ), but something that will check only the suffix 
+ * of the context path.
+ * 
+ * @author Ohad Redlich
+ *
+ */
+public class RegexRedirectResolver extends DefaultRedirectResolver
+{
+	/**
+	 * Whether the requested redirect URI "matches" the specified redirect URI. This implementation tests strict
+	 * equality.
+	 * 
+	 * @param requestedRedirect The requested redirect URI.
+	 * @param redirectUri The registered redirect URI.
+	 * @return Whether the requested redirect URI "matches" the specified redirect URI.
+	 */
+	@Override
+	protected boolean redirectMatches(String requestedRedirect, String redirectUri) 
+	{
+		return requestedRedirect.matches( redirectUri );
+	}
+
+}
