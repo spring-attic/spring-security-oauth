309ceede88ab2bdb2833ee5ed062c1eb3e2a91cd
OhadR <ohadr@watchdox.com>
2013-08-17 00:12:29 +0300

    Create TestRegexRedirectResolver.java

---
+++ spring-security-oauth2/src/test/java/org/springframework/security/oauth2/provider/code/TestRegexRedirectResolver.java
@@ -0,0 +1,40 @@
+package org.springframework.security.oauth2.provider.code;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.springframework.security.oauth2.provider.BaseClientDetails;
+
+public class TestRegexRedirectResolver
+{
+	private final RegexRedirectResolver resolver = new RegexRedirectResolver();
+	private final BaseClientDetails client = new BaseClientDetails();
+
+	{
+		client.setAuthorizedGrantTypes(Collections.singleton("authorization_code"));
+	}
+
+
+	@Test
+	public void testRedirectWatchdox() throws Exception 
+	{
+		Set<String> redirectUris = new HashSet<String>(Arrays.asList("http.*(watchdox.com).*"));
+		client.setRegisteredRedirectUri(redirectUris);
+		String requestedRedirect = "http://anywhere.watchdox.com/something";
+		assertEquals(requestedRedirect, resolver.resolveRedirect(requestedRedirect, client));
+	}
+
+	@Test
+	public void testRedirectBadWatchdox() throws Exception 
+	{
+		Set<String> redirectUris = new HashSet<String>(Arrays.asList("http.*(watchdox.com).*"));
+		client.setRegisteredRedirectUri(redirectUris);
+		String requestedRedirect = "http://anywhere.google.com/something";
+		assertEquals(requestedRedirect, resolver.resolveRedirect(requestedRedirect, client));
+	}
+}
