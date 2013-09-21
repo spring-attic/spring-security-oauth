487535ca949bb9914d8b510a4c46e00dec874f84
Casey Lucas <caseylucas@gmail.com>
2013-09-20 20:44:34 -0500

    change test cases to match updated error handling

--- samples/oauth2/sparklr/src/test/java/org/springframework/security/oauth2/provider/TestAdminEndpoints.java
+++ samples/oauth2/sparklr/src/test/java/org/springframework/security/oauth2/provider/TestAdminEndpoints.java
@@ -2,6 +2,9 @@ package org.springframework.security.oauth2.provider;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 
@@ -18,6 +21,8 @@ import org.springframework.security.oauth2.client.test.OAuth2ContextSetup;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InsufficientScopeException;
+import org.springframework.security.oauth2.common.exceptions.UserDeniedAuthorizationException;
 
 /**
  * @author Dave Syer
@@ -44,6 +49,7 @@ public class TestAdminEndpoints {
 	public void testRevokeTokenByUser() throws Exception {
 
 		OAuth2AccessToken token = context.getAccessToken();
+		String tokenValueBeforeDeletion = token.getValue();
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
@@ -55,11 +61,20 @@ public class TestAdminEndpoints {
 						.getRestTemplate()
 						.exchange(serverRunning.getUrl("/sparklr2/oauth/users/{user}/tokens/{token}"),
 								HttpMethod.DELETE, request, Void.class, "marissa", token.getValue()).getStatusCode());
-
-		ResponseEntity<String> result = serverRunning.getForString("/sparklr2/oauth/users/marissa/tokens", headers);
-		assertEquals(HttpStatus.UNAUTHORIZED, result.getStatusCode());
-		assertTrue(result.getBody().contains("invalid_token"));
-
+		try {
+			// The request above will delete the oauth token so that the next request will initially fail. However,
+			// the failure will be detected and a new access token will be obtained.  The new access token
+			// only has "write" scope and the requested resource needs "read" scope.  So, an insufficient_scope
+			// exception should be thrown.
+			ResponseEntity<String> result = serverRunning.getForString("/sparklr2/oauth/users/marissa/tokens", headers);
+			fail("Should have thrown an exception");
+		} catch (InsufficientScopeException ex) {
+			assertEquals(HttpStatus.FORBIDDEN.value(), ex.getHttpErrorCode());
+			assertEquals("insufficient_scope", ex.getOAuth2ErrorCode());
+			String secondTokenWithWriteOnlyScope = context.getOAuth2ClientContext().getAccessToken().getValue();
+			assertNotNull(secondTokenWithWriteOnlyScope);
+			assertFalse(secondTokenWithWriteOnlyScope.equals(tokenValueBeforeDeletion));
+		}
 	}
 
 	@Test
@@ -75,7 +90,13 @@ public class TestAdminEndpoints {
 	@Test
 	@OAuth2ContextConfiguration(ResourceOwnerReadOnly.class)
 	public void testCannotListTokensOfAnotherUser() throws Exception {
-		assertEquals(HttpStatus.FORBIDDEN, serverRunning.getStatusCode("/sparklr2/oauth/users/foo/tokens"));
+		try {
+			serverRunning.getStatusCode("/sparklr2/oauth/users/foo/tokens");
+			fail("Should have thrown an exception");
+		} catch (UserDeniedAuthorizationException ex) {
+			// assertEquals(HttpStatus.FORBIDDEN.value(), ex.getHttpErrorCode());
+			assertEquals("access_denied", ex.getOAuth2ErrorCode());
+		}
 	}
 
 	@Test
@@ -90,8 +111,13 @@ public class TestAdminEndpoints {
 	@Test
 	@OAuth2ContextConfiguration(ResourceOwnerReadOnly.class)
 	public void testUserCannotListTokensOfClient() throws Exception {
-		assertEquals(HttpStatus.FORBIDDEN,
-				serverRunning.getStatusCode("/sparklr2/oauth/clients/my-client-with-registered-redirect/tokens"));
+		try {
+			serverRunning.getStatusCode("/sparklr2/oauth/clients/my-client-with-registered-redirect/tokens");
+			fail("Should have thrown an exception");
+		} catch (UserDeniedAuthorizationException ex) {
+			// assertEquals(HttpStatus.FORBIDDEN.value(), ex.getHttpErrorCode());
+			assertEquals("access_denied", ex.getOAuth2ErrorCode());
+		}
 	}
 
 	static class ResourceOwnerReadOnly extends ResourceOwnerPasswordResourceDetails {
