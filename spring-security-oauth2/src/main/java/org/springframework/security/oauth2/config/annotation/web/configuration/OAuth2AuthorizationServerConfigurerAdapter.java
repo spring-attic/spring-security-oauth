a82c645b119712b33902542410b33a60e53d3b77
Dave Syer <dsyer@gopivotal.com>
2014-02-14 17:13:23 +0000

    More care with proxies in @Configuration
    Apparently the rule of thumb is: if a @Bean uses a configurer
    to grab one of it's dependencies then it should be a lazy proxy
    in case it is used before the configurer has initialized.
    
    In addition we find that an Application.class that extends
    OAuth2AuthorizationServerConfigurerAdapter directly is problematic,
    but if you nest it it works fine. Problems manifest themselves in
    objects that ought to be proxies not being proxied (i.e. early
    instantiation going on somewhere). This has no impact on
    Spring OAuth (yet), but it affects applications that use it.

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/config/annotation/web/configuration/OAuth2AuthorizationServerConfigurerAdapter.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/config/annotation/web/configuration/OAuth2AuthorizationServerConfigurerAdapter.java
@@ -31,6 +31,7 @@ import org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoi
 import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpointHandlerMapping;
 import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
 import org.springframework.security.oauth2.provider.endpoint.WhitelabelApprovalEndpoint;
+import org.springframework.security.oauth2.provider.implicit.ImplicitGrantService;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -38,94 +39,102 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 /**
  * @author Rob Winch
  * @author Dave Syer
- *
+ * 
  */
 @Configuration
 public abstract class OAuth2AuthorizationServerConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public AuthorizationEndpoint authorizationEndpoint() throws Exception {
-        AuthorizationEndpoint authorizationEndpoint = new AuthorizationEndpoint();
-        authorizationEndpoint.setTokenGranter(tokenGranter());
-        authorizationEndpoint.setClientDetailsService(clientDetailsService());
-        authorizationEndpoint.setAuthorizationCodeServices(authorizationCodeServices());
-        authorizationEndpoint.setUserApprovalHandler(userApprovalHandler());
-        authorizationEndpoint.setImplicitGrantService(authorizationServerConfigurer().getImplicitGrantService());
-        return authorizationEndpoint;
-    }
+	@Bean
+	public AuthorizationEndpoint authorizationEndpoint() throws Exception {
+		AuthorizationEndpoint authorizationEndpoint = new AuthorizationEndpoint();
+		authorizationEndpoint.setTokenGranter(tokenGranter());
+		authorizationEndpoint.setClientDetailsService(clientDetailsService());
+		authorizationEndpoint.setAuthorizationCodeServices(authorizationCodeServices());
+		authorizationEndpoint.setUserApprovalHandler(userApprovalHandler());
+		authorizationEndpoint.setImplicitGrantService(getImplicitGrantService());
+		return authorizationEndpoint;
+	}
 
-    @Bean
-    @Lazy
-    @Scope(proxyMode=ScopedProxyMode.INTERFACES)
-    public ConsumerTokenServices consumerTokenServices() throws Exception {
-        return authorizationServerConfigurer().getConsumerTokenServices();
-    }
-
-    @Bean
-    public TokenEndpoint tokenEndpoint() throws Exception {
-        TokenEndpoint tokenEndpoint = new TokenEndpoint();
-        tokenEndpoint.setClientDetailsService(clientDetailsService());
-        tokenEndpoint.setTokenGranter(tokenGranter());
-        return tokenEndpoint;
-    }
-
-    @Bean
-    @Lazy
-    @Scope(proxyMode=ScopedProxyMode.INTERFACES)
-    public OAuth2RequestFactory oauth2RequestFactory() throws Exception {
-        return authorizationServerConfigurer().getOAuth2RequestFactory();
-    }
-
-    @Bean
-    @Lazy
-    @Scope(proxyMode=ScopedProxyMode.INTERFACES)
-    public TokenStore tokenStore() throws Exception {
-        return authorizationServerConfigurer().getTokenStore();
-    }
-
-    @Bean
+	@Bean
 	@Lazy
-	@Scope(proxyMode=ScopedProxyMode.INTERFACES)
-    public UserApprovalHandler userApprovalHandler() throws Exception {
-        return authorizationServerConfigurer().getUserApprovalHandler();
-    }
+	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
+	public ImplicitGrantService getImplicitGrantService() throws Exception {
+		return authorizationServerConfigurer().getImplicitGrantService();
+	}
 
-    protected AuthorizationServerTokenServices tokenServices() throws Exception {
-        return authorizationServerConfigurer().getTokenServices();
-    }
+	@Bean
+	@Lazy
+	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
+	public ConsumerTokenServices consumerTokenServices() throws Exception {
+		return authorizationServerConfigurer().getConsumerTokenServices();
+	}
 
-    @Bean
-    public WhitelabelApprovalEndpoint approvalEndpoint() {
-        return new WhitelabelApprovalEndpoint();
-    }
+	@Bean
+	public TokenEndpoint tokenEndpoint() throws Exception {
+		TokenEndpoint tokenEndpoint = new TokenEndpoint();
+		tokenEndpoint.setClientDetailsService(clientDetailsService());
+		tokenEndpoint.setTokenGranter(tokenGranter());
+		return tokenEndpoint;
+	}
 
-    @Bean
-    public FrameworkEndpointHandlerMapping endpointHandlerMapping() {
-        return new FrameworkEndpointHandlerMapping();
-    }
+	@Bean
+	@Lazy
+	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
+	public OAuth2RequestFactory oauth2RequestFactory() throws Exception {
+		return authorizationServerConfigurer().getOAuth2RequestFactory();
+	}
 
-    @Bean
-    @Lazy
-    @Scope(proxyMode=ScopedProxyMode.INTERFACES)
-    public ClientDetailsService clientDetailsService() throws Exception {
-        return getHttp().getSharedObject(ClientDetailsService.class);
-    }
+	@Bean
+	@Lazy
+	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
+	public TokenStore tokenStore() throws Exception {
+		return authorizationServerConfigurer().getTokenStore();
+	}
 
-    @Bean
-    @Lazy
-    @Scope(proxyMode=ScopedProxyMode.INTERFACES)
-    public AuthorizationCodeServices authorizationCodeServices() throws Exception {
-        return authorizationServerConfigurer().getAuthorizationCodeServices();
-    }
+	@Bean
+	@Lazy
+	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
+	public UserApprovalHandler userApprovalHandler() throws Exception {
+		return authorizationServerConfigurer().getUserApprovalHandler();
+	}
 
-    @Bean
-    @Lazy
-    @Scope(proxyMode=ScopedProxyMode.INTERFACES)
-    public TokenGranter tokenGranter() throws Exception {
-        return authorizationServerConfigurer().getTokenGranter();
-    }
+	protected AuthorizationServerTokenServices tokenServices() throws Exception {
+		return authorizationServerConfigurer().getTokenServices();
+	}
 
-    private OAuth2AuthorizationServerConfigurer authorizationServerConfigurer() throws Exception {
-        return getHttp().getConfigurer(OAuth2AuthorizationServerConfigurer.class);
-    }
+	@Bean
+	public WhitelabelApprovalEndpoint approvalEndpoint() {
+		return new WhitelabelApprovalEndpoint();
+	}
+
+	@Bean
+	public FrameworkEndpointHandlerMapping endpointHandlerMapping() {
+		return new FrameworkEndpointHandlerMapping();
+	}
+
+	@Bean
+	@Lazy
+	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
+	public ClientDetailsService clientDetailsService() throws Exception {
+		return getHttp().getSharedObject(ClientDetailsService.class);
+	}
+
+	@Bean
+	@Lazy
+	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
+	public AuthorizationCodeServices authorizationCodeServices() throws Exception {
+		return authorizationServerConfigurer().getAuthorizationCodeServices();
+	}
+
+	@Bean
+	@Lazy
+	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
+	public TokenGranter tokenGranter() throws Exception {
+		return authorizationServerConfigurer().getTokenGranter();
+	}
+
+	private OAuth2AuthorizationServerConfigurer authorizationServerConfigurer() throws Exception {
+		return getHttp().getConfigurer(OAuth2AuthorizationServerConfigurer.class);
+	}
+
 }
