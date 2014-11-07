f6e0acf94bb90a525598d6e7edb24c923b0f9723
sgyyz <itnote.yyz@gamil.com>
2014-11-07 23:06:59 +0800

    Add @Qualifier for ConsumerTokenServices.
    This shouldn't be necessary and it works for me, but users
    have reported that it is needed. When someone has time we
    might be able to figure out why.
    
    Fixes gh-298, fixes gh-303

--- samples/oauth2/sparklr/src/main/java/org/springframework/security/oauth/examples/sparklr/config/WebMvcConfig.java
+++ samples/oauth2/sparklr/src/main/java/org/springframework/security/oauth/examples/sparklr/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -39,65 +40,66 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
 
-   @Bean
-    public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
-        ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
-        contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
+	@Bean
+	public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
+		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+		contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
 
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(".jsp");
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
 
-        MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
-        defaultView.setExtractValueFromSingleKeyModel(true);
+		MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
+		defaultView.setExtractValueFromSingleKeyModel(true);
 
-        ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
-        contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
-        contentViewResolver.setViewResolvers(Arrays.<ViewResolver>asList(viewResolver));
-        contentViewResolver.setDefaultViews(Arrays.<View>asList(defaultView));
-        return contentViewResolver;
-    }
-
-   
-    @Bean
-    public PhotoServiceUserController photoServiceUserController(PhotoService photoService) {
-   	   PhotoServiceUserController photoServiceUserController = new PhotoServiceUserController();
-       return photoServiceUserController;
-    }
-   
-    @Bean
-    public PhotoController photoController(PhotoService photoService) {
-        PhotoController photoController = new PhotoController();
-        photoController.setPhotoService(photoService);
-        return photoController;
-    }
-
-    @Bean
-    public AccessConfirmationController accessConfirmationController(ClientDetailsService clientDetailsService, ApprovalStore approvalStore) {
-        AccessConfirmationController accessConfirmationController = new AccessConfirmationController();
-        accessConfirmationController.setClientDetailsService(clientDetailsService);
-        accessConfirmationController.setApprovalStore(approvalStore);
-        return accessConfirmationController;
-    }
-
-    @Bean
-    public PhotoServiceImpl photoServices() {
-        List<PhotoInfo> photos = new ArrayList<PhotoInfo>();
-        photos.add(createPhoto("1", "marissa"));
-        photos.add(createPhoto("2", "paul"));
-        photos.add(createPhoto("3", "marissa"));
-        photos.add(createPhoto("4", "paul"));
-        photos.add(createPhoto("5", "marissa"));
-        photos.add(createPhoto("6", "paul"));
-
-        PhotoServiceImpl photoServices = new PhotoServiceImpl();
-        photoServices.setPhotos(photos);
-        return photoServices;
-    }
-
+		ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
+		contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+		contentViewResolver.setViewResolvers(Arrays.<ViewResolver> asList(viewResolver));
+		contentViewResolver.setDefaultViews(Arrays.<View> asList(defaultView));
+		return contentViewResolver;
+	}
 
 	@Bean
-	public AdminController adminController(TokenStore tokenStore, ConsumerTokenServices tokenServices,
+	public PhotoServiceUserController photoServiceUserController(PhotoService photoService) {
+		PhotoServiceUserController photoServiceUserController = new PhotoServiceUserController();
+		return photoServiceUserController;
+	}
+
+	@Bean
+	public PhotoController photoController(PhotoService photoService) {
+		PhotoController photoController = new PhotoController();
+		photoController.setPhotoService(photoService);
+		return photoController;
+	}
+
+	@Bean
+	public AccessConfirmationController accessConfirmationController(ClientDetailsService clientDetailsService,
+			ApprovalStore approvalStore) {
+		AccessConfirmationController accessConfirmationController = new AccessConfirmationController();
+		accessConfirmationController.setClientDetailsService(clientDetailsService);
+		accessConfirmationController.setApprovalStore(approvalStore);
+		return accessConfirmationController;
+	}
+
+	@Bean
+	public PhotoServiceImpl photoServices() {
+		List<PhotoInfo> photos = new ArrayList<PhotoInfo>();
+		photos.add(createPhoto("1", "marissa"));
+		photos.add(createPhoto("2", "paul"));
+		photos.add(createPhoto("3", "marissa"));
+		photos.add(createPhoto("4", "paul"));
+		photos.add(createPhoto("5", "marissa"));
+		photos.add(createPhoto("6", "paul"));
+
+		PhotoServiceImpl photoServices = new PhotoServiceImpl();
+		photoServices.setPhotos(photos);
+		return photoServices;
+	}
+
+	// N.B. the @Qualifier here should not be necessary (gh-298) but lots of users report needing it.
+	@Bean
+	public AdminController adminController(TokenStore tokenStore,
+			@Qualifier("consumerTokenServices") ConsumerTokenServices tokenServices,
 			SparklrUserApprovalHandler userApprovalHandler) {
 		AdminController adminController = new AdminController();
 		adminController.setTokenStore(tokenStore);
@@ -106,18 +108,17 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		return adminController;
 	}
 
-   private PhotoInfo createPhoto(String id, String userId) {
-        PhotoInfo photo = new PhotoInfo();
-        photo.setId(id);
-        photo.setName("photo" + id + ".jpg");
-        photo.setUserId(userId);
-        photo.setResourceURL("/org/springframework/security/oauth/examples/sparklr/impl/resources/" + photo.getName());
-        return photo;
-    }
+	private PhotoInfo createPhoto(String id, String userId) {
+		PhotoInfo photo = new PhotoInfo();
+		photo.setId(id);
+		photo.setName("photo" + id + ".jpg");
+		photo.setUserId(userId);
+		photo.setResourceURL("/org/springframework/security/oauth/examples/sparklr/impl/resources/" + photo.getName());
+		return photo;
+	}
 
-    @Override
-    public void configureDefaultServletHandling(
-            DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 }
