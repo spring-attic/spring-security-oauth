9558f623ce96dc98e30bdee093985309dcf504da
Chris Campo <christopher.campo@aspect.com>
2014-12-09 19:13:05 -0500

    Add permitAll() to OPTIONS requests on token endpoint to support CORS

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/config/annotation/web/configuration/AuthorizationServerSecurityConfiguration.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/config/annotation/web/configuration/AuthorizationServerSecurityConfiguration.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configuration.ClientDetailsServiceConfiguration;
@@ -68,6 +69,9 @@ public class AuthorizationServerSecurityConfiguration extends WebSecurityConfigu
 		String checkTokenPath = handlerMapping.getServletPath("/oauth/check_token");
 		// @formatter:off
 		http
+			.authorizeRequests()
+				.antMatchers(HttpMethod.OPTIONS, tokenEndpointPath).permitAll()
+			.and()
         	.authorizeRequests()
             	.antMatchers(tokenEndpointPath).fullyAuthenticated()
             	.antMatchers(tokenKeyPath).access(configurer.getTokenKeyAccess())
