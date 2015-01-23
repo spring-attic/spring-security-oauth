7043cd702299cfacb677273d041579ebbe8bdc74
Dave Syer <dsyer@gopivotal.com>
2015-01-23 14:01:55 +0000

    Remove unused security filter chain
    Fixes gh-364

--- samples/oauth2/sparklr/src/main/java/org/springframework/security/oauth/examples/sparklr/config/OAuth2ServerConfig.java
+++ samples/oauth2/sparklr/src/main/java/org/springframework/security/oauth/examples/sparklr/config/OAuth2ServerConfig.java
@@ -23,11 +23,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth.examples.sparklr.oauth.SparklrUserApprovalHandler;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -55,25 +53,6 @@ public class OAuth2ServerConfig {
 	private static final String SPARKLR_RESOURCE_ID = "sparklr";
 
 	@Configuration
-	@Order(10)
-	protected static class UiResourceConfiguration extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-				.requestMatchers().antMatchers("/photos/**","/me")
-			.and()
-				.authorizeRequests()
-				.antMatchers("/me").access("hasRole('ROLE_USER')")
-				.antMatchers("/photos").access("hasRole('ROLE_USER')")
-				.antMatchers("/photos/trusted/**").access("hasRole('ROLE_USER')")
-				.antMatchers("/photos/user/**").access("hasRole('ROLE_USER')")
-				.antMatchers("/photos/**").access("hasRole('ROLE_USER')");
-			// @formatter:on
-		}
-	}
-
-	@Configuration
 	@EnableResourceServer
 	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
@@ -90,10 +69,10 @@ public class OAuth2ServerConfig {
 			.and()
 				.authorizeRequests()
 					.antMatchers("/me").access("#oauth2.hasScope('read')")					
-					.antMatchers("/photos").access("#oauth2.hasScope('read') or hasRole('ROLE_USER')")                                        
+					.antMatchers("/photos").access("#oauth2.hasScope('read') or (!#oauth2.isOAuth() and hasRole('ROLE_USER'))")                                        
 					.antMatchers("/photos/trusted/**").access("#oauth2.hasScope('trust')")
 					.antMatchers("/photos/user/**").access("#oauth2.hasScope('trust')")					
-					.antMatchers("/photos/**").access("#oauth2.hasScope('read') or hasRole('ROLE_USER')")
+					.antMatchers("/photos/**").access("#oauth2.hasScope('read') or (!#oauth2.isOAuth() and hasRole('ROLE_USER'))")
 					.regexMatchers(HttpMethod.DELETE, "/oauth/users/([^/].*?)/tokens/.*")
 						.access("#oauth2.clientHasRole('ROLE_CLIENT') and (hasRole('ROLE_USER') or #oauth2.isClient()) and #oauth2.hasScope('write')")
 					.regexMatchers(HttpMethod.GET, "/oauth/clients/([^/].*?)/users/.*")
