49f59e409f56709c228c06d515fcfba66272fef1
Aliaksandr Autayeu <aliaksandr@autayeu.com>
2015-02-04 11:20:28 +0100

    added serialVersionUID after @alexzm1 comment.

--- spring-security-oauth/src/main/java/org/springframework/security/oauth/common/OAuthParameters.java
+++ spring-security-oauth/src/main/java/org/springframework/security/oauth/common/OAuthParameters.java
@@ -21,6 +21,8 @@ import java.io.Serializable;
  */
 public class OAuthParameters implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
 	private String consumerKey;
 	private String token;
 	private String tokenSecret;
