234ec98f88f9ef5246381beed21eb7345d4aa82d
Dave Syer <dsyer@pivotal.io>
2015-10-06 13:45:55 +0100

    Fix SpelView for Spring 4.2 and empty context path

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/SpelView.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/SpelView.java
@@ -64,8 +64,9 @@ class SpelView implements View {
 	public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		Map<String, Object> map = new HashMap<String, Object>(model);
-		map.put("path", (Object) ServletUriComponentsBuilder.fromContextPath(request).build()
-				.getPath());
+		String path = ServletUriComponentsBuilder.fromContextPath(request).build()
+				.getPath();
+		map.put("path", (Object) path==null ? "" : path);
 		context.setRootObject(map);
 		String result = helper.replacePlaceholders(template, resolver);
 		response.setContentType(getContentType());
