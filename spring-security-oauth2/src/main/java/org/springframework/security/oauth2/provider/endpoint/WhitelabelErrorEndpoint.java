8fdd4618a6eafe488266c8b461a249d7796bb4cb
michaeltecourt <michael.tecourt@gmail.com>
2015-11-19 10:43:43 +0100

    WhiteLabelErrorEndpoint XSS vulnerability fix
    Some OAuth2Exceptions may contain client input (invalid grant types,
    etc), and need to be escaped before getting rendered by the default
    error endpoint.
    
     * Add HTML escaping for OAuth2Exception errors
     * Other type of "error" objects result in a default message (SpEL
    ${error.summary} was not going to work anyway)
     * Unit test

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/WhitelabelErrorEndpoint.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/WhitelabelErrorEndpoint.java
@@ -1,33 +1,40 @@
 package org.springframework.security.oauth2.provider.endpoint;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.HtmlUtils;
 
 /**
  * Controller for displaying the error page for the authorization server.
- * 
+ *
  * @author Dave Syer
  */
 @FrameworkEndpoint
 public class WhitelabelErrorEndpoint {
 
+	private static final String ERROR = "<html><body><h1>OAuth Error</h1><p>${errorSummary}</p></body></html>";
+
 	@RequestMapping("/oauth/error")
-	public ModelAndView handleError(HttpServletRequest request) {
+	public ModelAndView handleError(final HttpServletRequest request) {
 		Map<String, Object> model = new HashMap<String, Object>();
 		Object error = request.getAttribute("error");
-		if (error==null) {
-			error = Collections.singletonMap("summary", "Unknown error");
+		// The error summary may contain malicious user input,
+		// it needs to be escaped to prevent XSS
+		String errorSummary;
+		if (error instanceof OAuth2Exception) {
+			OAuth2Exception oauthError = (OAuth2Exception) error;
+			errorSummary = HtmlUtils.htmlEscape(oauthError.getSummary());
 		}
-		model.put("error", error);
+		else {
+			errorSummary = "Unknown error";
+		}
+		model.put("errorSummary", errorSummary);
 		return new ModelAndView(new SpelView(ERROR), model);
 	}
-
-	private static String ERROR = "<html><body><h1>OAuth Error</h1><p>${error.summary}</p></body></html>";
-
 }
