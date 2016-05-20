a0e2591163a9c2991648473b8f503dde11fff18c
Joe Grandja <jgrandja@pivotal.io>
2016-05-20 12:42:13 -0400

    Remove keys in RedisTokenStore on revoke
    Fixes gh-572

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/store/redis/RedisTokenStore.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/store/redis/RedisTokenStore.java
@@ -305,12 +305,14 @@ public class RedisTokenStore implements TokenStore {
 
 	public void removeRefreshToken(String tokenValue) {
 		byte[] refreshKey = serializeKey(REFRESH + tokenValue);
+		byte[] refreshAuthKey = serializeKey(REFRESH_AUTH + tokenValue);
 		byte[] refresh2AccessKey = serializeKey(REFRESH_TO_ACCESS + tokenValue);
 		byte[] access2RefreshKey = serializeKey(ACCESS_TO_REFRESH + tokenValue);
 		RedisConnection conn = getConnection();
 		try {
 			conn.openPipeline();
 			conn.del(refreshKey);
+			conn.del(refreshAuthKey);
 			conn.del(refresh2AccessKey);
 			conn.del(access2RefreshKey);
 			conn.closePipeline();
