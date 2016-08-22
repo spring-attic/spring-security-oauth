ef8d78403d551ca9c23c6de4db0aa6aae2e24d79
Daniel <wrightdaniel86@gmail.com>
2016-08-22 16:55:32 +0100

    Fix typo
    Closes gh-827

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/config/annotation/web/configuration/AuthorizationServerEndpointsConfiguration.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/config/annotation/web/configuration/AuthorizationServerEndpointsConfiguration.java
@@ -82,7 +82,7 @@ public class AuthorizationServerEndpointsConfiguration {
 			try {
 				configurer.configure(endpoints);
 			} catch (Exception e) {
-				throw new IllegalStateException("Cannot configure enpdoints", e);
+				throw new IllegalStateException("Cannot configure endpoints", e);
 			}
 		}
 		endpoints.setClientDetailsService(clientDetailsService);
