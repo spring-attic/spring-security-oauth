dd369f2b48fe46a6ac32e57abffa2cdbfa75b392
Dan Dudley <dan@pithoslabs.com>
2016-10-07 14:47:49 -0500

    Sort scopes before generating key
    Updated to always sort the scopes set before generating the checksum
    to ensure regardless where we got the scope set from the checksum
    outcome will remain the same
    
    Fixes gh-874

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/DefaultAuthenticationKeyGenerator.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/DefaultAuthenticationKeyGenerator.java
@@ -12,16 +12,14 @@
  */
 package org.springframework.security.oauth2.provider.token;
 
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
+import org.springframework.security.oauth2.common.util.MapDigester;
 import org.springframework.security.oauth2.common.util.OAuth2Utils;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeSet;
 
 /**
  * Basic key generator taking into account the client id, scope, resource ids and username (principal name) if they
@@ -38,6 +36,8 @@ public class DefaultAuthenticationKeyGenerator implements AuthenticationKeyGener
 
 	private static final String USERNAME = "username";
 
+	private MapDigester mapDigester = new MapDigester();
+
 	public String extractKey(OAuth2Authentication authentication) {
 		Map<String, String> values = new LinkedHashMap<String, String>();
 		OAuth2Request authorizationRequest = authentication.getOAuth2Request();
@@ -46,23 +46,9 @@ public class DefaultAuthenticationKeyGenerator implements AuthenticationKeyGener
 		}
 		values.put(CLIENT_ID, authorizationRequest.getClientId());
 		if (authorizationRequest.getScope() != null) {
-			values.put(SCOPE, OAuth2Utils.formatParameterList(authorizationRequest.getScope()));
+			values.put(SCOPE, OAuth2Utils.formatParameterList(new TreeSet<String>(authorizationRequest.getScope())));
 		}
-		MessageDigest digest;
-		try {
-			digest = MessageDigest.getInstance("MD5");
-		}
-		catch (NoSuchAlgorithmException e) {
-			throw new IllegalStateException("MD5 algorithm not available.  Fatal (should be in the JDK).");
-		}
-
-		try {
-			byte[] bytes = digest.digest(values.toString().getBytes("UTF-8"));
-			return String.format("%032x", new BigInteger(1, bytes));
-		}
-		catch (UnsupportedEncodingException e) {
-			throw new IllegalStateException("UTF-8 encoding not available.  Fatal (should be in the JDK).");
-		}
+		return mapDigester.digest(values);
 	}
 
 }
