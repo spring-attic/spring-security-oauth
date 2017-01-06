746d704c280d985894cef167a290ad77d616b60f
redmark <redmark@gmail.com>
2017-01-07 03:12:25 +0800

    Update DefaultTokenServices.java

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/DefaultTokenServices.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/DefaultTokenServices.java
@@ -146,7 +146,7 @@ public class DefaultTokenServices implements AuthorizationServerTokenServices, R
 		if (this.authenticationManager != null && !authentication.isClientOnly()) {
 			// The client has already been authenticated, but the user authentication might be old now, so give it a
 			// chance to re-authenticate.
-			Authentication user = new PreAuthenticatedAuthenticationToken(authentication.getUserAuthentication(), "", authentication.getAuthorities());
+			Authentication user = new PreAuthenticatedAuthenticationToken(authentication.getUserAuthentication().getPrincipal(), "", authentication.getAuthorities());
 			user = authenticationManager.authenticate(user);
 			Object details = authentication.getDetails();
 			authentication = new OAuth2Authentication(authentication.getOAuth2Request(), user);
