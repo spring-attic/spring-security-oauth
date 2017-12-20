8fc19343fdc21671ba036c45627b9b7f51637b4b
Joe Grandja <jgrandja@pivotal.io>
2017-12-19 22:15:48 -0500

    Polish #1161 Add Elliptic Curve verification support for spring-security-jwt

--- spring-security-jwt/src/main/java/org/springframework/security/jwt/crypto/sign/EllipticCurveKeyHelper.java
+++ spring-security-jwt/src/main/java/org/springframework/security/jwt/crypto/sign/EllipticCurveKeyHelper.java
@@ -1,14 +1,17 @@
 /*
- * Copyright 2006-2011 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.security.jwt.crypto.sign;
 
@@ -24,24 +27,30 @@ import java.security.spec.ECPoint;
 import java.security.spec.ECPublicKeySpec;
 
 /**
+ * Utilities for Elliptic Curve keys.
+ *
  * @author Michael Duergner
+ * @since 2.3
  */
-class EllipticCurveKeyHelper {
+final class EllipticCurveKeyHelper {
 
-    static ECPublicKey createPublicKey(final BigInteger x, final BigInteger y, final String curve) {
-        try {
-            ECNamedCurveParameterSpec curveParameterSpec = ECNamedCurveTable.getParameterSpec(curve);
-            ECParameterSpec parameterSpec = new ECNamedCurveSpec(
-                    curveParameterSpec.getName(),
-                    curveParameterSpec.getCurve(),
-                    curveParameterSpec.getG(),
-                    curveParameterSpec.getN());
-            ECPublicKeySpec publicKeySpec = new ECPublicKeySpec(new ECPoint(x, y), parameterSpec);
+	static ECPublicKey createPublicKey(final BigInteger x, final BigInteger y, final String curve) {
+		ECNamedCurveParameterSpec curveParameterSpec;
+		if ((curveParameterSpec = ECNamedCurveTable.getParameterSpec(curve)) == null) {
+			throw new IllegalArgumentException("Unsupported named curve: " + curve);
+		}
 
-            return (ECPublicKey) KeyFactory.getInstance("EC").generatePublic(publicKeySpec);
-        }
-        catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+		ECParameterSpec parameterSpec = new ECNamedCurveSpec(
+				curveParameterSpec.getName(),
+				curveParameterSpec.getCurve(),
+				curveParameterSpec.getG(),
+				curveParameterSpec.getN());
+		ECPublicKeySpec publicKeySpec = new ECPublicKeySpec(new ECPoint(x, y), parameterSpec);
+
+		try {
+			return (ECPublicKey) KeyFactory.getInstance("EC").generatePublic(publicKeySpec);
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 }
--- spring-security-jwt/src/main/java/org/springframework/security/jwt/crypto/sign/EllipticCurveVerifier.java
+++ spring-security-jwt/src/main/java/org/springframework/security/jwt/crypto/sign/EllipticCurveVerifier.java
@@ -1,14 +1,17 @@
 /*
- * Copyright 2006-2011 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.security.jwt.crypto.sign;
 
@@ -18,107 +21,42 @@ import java.security.Signature;
 import java.security.interfaces.ECPublicKey;
 
 /**
+ * Verifies signatures using an Elliptic Curve public key.
+ *
  * @author Michael Duergner
+ * @since 2.3
  */
 public class EllipticCurveVerifier implements SignatureVerifier {
+	private final ECPublicKey key;
+	private final String algorithm;
 
-    private final ECPublicKey key;
+	public EllipticCurveVerifier(final BigInteger x, final BigInteger y,
+								 final String curve, final String algorithm) {
+		this(EllipticCurveKeyHelper.createPublicKey(x, y, curve), algorithm);
+	}
 
-    private final String algorithm;
+	public EllipticCurveVerifier(final ECPublicKey key, final String algorithm) {
+		this.key = key;
+		this.algorithm = algorithm;
+	}
 
-    public EllipticCurveVerifier(final BigInteger x, final BigInteger y, final String curve, final String algorithm) {
-        this(EllipticCurveKeyHelper.createPublicKey(x, y, curve), algorithm);
-    }
+	@Override
+	public String algorithm() {
+		return this.algorithm;
+	}
 
-    public EllipticCurveVerifier(final ECPublicKey key, final String algorithm) {
-        this.key = key;
-        this.algorithm = algorithm;
-    }
+	@Override
+	public void verify(byte[] content, byte[] sig) {
+		try {
+			Signature signature = Signature.getInstance(this.algorithm);
+			signature.initVerify(this.key);
+			signature.update(content);
 
-    @Override
-    public String algorithm() {
-        return algorithm;
-    }
-
-    @Override
-    public void verify(byte[] content, byte[] sig) {
-        try {
-            Signature signature = Signature.getInstance(algorithm);
-            signature.initVerify(key);
-            signature.update(content);
-
-            if (!signature.verify(transcodeSignatureToDER(sig))) {
-                throw new InvalidSignatureException("EC Signature did not match content");
-            }
-        }
-        catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private byte[] transcodeSignatureToDER(byte[] jwsSignature) throws GeneralSecurityException {
-
-        // Adapted from org.apache.xml.security.algorithms.implementations.SignatureECDSA
-
-        int rawLen = jwsSignature.length / 2;
-
-        int i;
-
-        for (i = rawLen; (i > 0) && (jwsSignature[rawLen - i] == 0); i--) {
-            // do nothing
-        }
-
-        int j = i;
-
-        if (jwsSignature[rawLen - i] < 0) {
-            j += 1;
-        }
-
-        int k;
-
-        for (k = rawLen; (k > 0) && (jwsSignature[2 * rawLen - k] == 0); k--) {
-            // do nothing
-        }
-
-        int l = k;
-
-        if (jwsSignature[2 * rawLen - k] < 0) {
-            l += 1;
-        }
-
-        int len = 2 + j + 2 + l;
-
-        if (len > 255) {
-            throw new GeneralSecurityException("Invalid ECDSA signature format");
-        }
-
-        int offset;
-
-        final byte derSignature[];
-
-        if (len < 128) {
-            derSignature = new byte[2 + 2 + j + 2 + l];
-            offset = 1;
-        } else {
-            derSignature = new byte[3 + 2 + j + 2 + l];
-            derSignature[1] = (byte) 0x81;
-            offset = 2;
-        }
-
-        derSignature[0] = 48;
-        derSignature[offset++] = (byte) len;
-        derSignature[offset++] = 2;
-        derSignature[offset++] = (byte) j;
-
-        System.arraycopy(jwsSignature, rawLen - i, derSignature, (offset + j) - i, i);
-
-        offset += j;
-
-        derSignature[offset++] = 2;
-        derSignature[offset++] = (byte) l;
-
-        System.arraycopy(jwsSignature, 2 * rawLen - k, derSignature, (offset + l) - k, k);
-
-        return derSignature;
-    }
+			if (!signature.verify(EllipticCurveSignatureHelper.transcodeSignatureToDER(sig))) {
+				throw new InvalidSignatureException("EC Signature did not match content");
+			}
+		} catch (GeneralSecurityException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 }
