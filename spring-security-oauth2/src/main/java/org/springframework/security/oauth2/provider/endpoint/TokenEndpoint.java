a25c107e9a5c6c71711ce67de03bd690f6b87a97
leandrobarcellos <lmb.leandro@gmail.com>
2018-01-25 12:37:38 -0200

    Update TokenEndpoint.java

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/TokenEndpoint.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/TokenEndpoint.java
@@ -49,6 +49,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody
 
 /**
  * <p>
