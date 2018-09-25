03b6ef454ba604058df9b0b26faf1a28c78d6cf8
wangjinzhao <bigbaldy@163.com>
2018-09-25 15:34:25 +0800

    fix accessToken to ThreadLocal

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/client/DefaultOAuth2ClientContext.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/client/DefaultOAuth2ClientContext.java
@@ -10,51 +10,51 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 /**
  * The OAuth 2 security context (for a specific user or client or combination thereof).
- * 
+ *
  * @author Dave Syer
  */
 public class DefaultOAuth2ClientContext implements OAuth2ClientContext, Serializable {
 
-	private static final long serialVersionUID = 914967629530462926L;
+    private static final long serialVersionUID = 914967629530462926L;
 
-	private OAuth2AccessToken accessToken;
+    private ThreadLocal<OAuth2AccessToken> accessToken = new ThreadLocal<>();
 
-	private AccessTokenRequest accessTokenRequest;
+    private AccessTokenRequest accessTokenRequest;
 
-	private Map<String, Object> state = new HashMap<String, Object>();
+    private Map<String, Object> state = new HashMap<String, Object>();
 
-	public DefaultOAuth2ClientContext() {
-		this(new DefaultAccessTokenRequest());
-	}
+    public DefaultOAuth2ClientContext() {
+        this(new DefaultAccessTokenRequest());
+    }
 
-	public DefaultOAuth2ClientContext(AccessTokenRequest accessTokenRequest) {
-		this.accessTokenRequest = accessTokenRequest;
-	}
+    public DefaultOAuth2ClientContext(AccessTokenRequest accessTokenRequest) {
+        this.accessTokenRequest = accessTokenRequest;
+    }
 
-	public DefaultOAuth2ClientContext(OAuth2AccessToken accessToken) {
-		this.accessToken = accessToken;
-		this.accessTokenRequest = new DefaultAccessTokenRequest();
-	}
+    public DefaultOAuth2ClientContext(OAuth2AccessToken accessToken) {
+        this.accessToken.set(accessToken);
+        this.accessTokenRequest = new DefaultAccessTokenRequest();
+    }
 
-	public OAuth2AccessToken getAccessToken() {
-		return accessToken;
-	}
+    public OAuth2AccessToken getAccessToken() {
+        return accessToken.get();
+    }
 
-	public void setAccessToken(OAuth2AccessToken accessToken) {
-		this.accessToken = accessToken;
-		this.accessTokenRequest.setExistingToken(accessToken);
-	}
+    public void setAccessToken(OAuth2AccessToken accessToken) {
+        this.accessToken.set(accessToken);
+        this.accessTokenRequest.setExistingToken(accessToken);
+    }
 
-	public AccessTokenRequest getAccessTokenRequest() {
-		return accessTokenRequest;
-	}
+    public AccessTokenRequest getAccessTokenRequest() {
+        return accessTokenRequest;
+    }
 
-	public void setPreservedState(String stateKey, Object preservedState) {
-		state.put(stateKey, preservedState);
-	}
+    public void setPreservedState(String stateKey, Object preservedState) {
+        state.put(stateKey, preservedState);
+    }
 
-	public Object removePreservedState(String stateKey) {
-		return state.remove(stateKey);
-	}
+    public Object removePreservedState(String stateKey) {
+        return state.remove(stateKey);
+    }
 
 }
