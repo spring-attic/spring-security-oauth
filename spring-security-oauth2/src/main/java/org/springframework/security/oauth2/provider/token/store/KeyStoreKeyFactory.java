fd7be8ef4520f6c5a71de8a520c3b67555281428
JC Pretorius <jc.pretorius@standardbank.co.za>
2018-11-19 12:21:57 +0200

    KeyStoreKeyFactory: InputStream not being closed
    https://github.com/spring-projects/spring-security-oauth/issues/1527

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/store/KeyStoreKeyFactory.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/store/KeyStoreKeyFactory.java
@@ -48,24 +48,34 @@ public class KeyStoreKeyFactory {
 		return getKeyPair(alias, password);
 	}
 
-	public KeyPair getKeyPair(String alias, char[] password) {
-		try {
-			synchronized (lock) {
-				if (store == null) {
-					synchronized (lock) {
-						store = KeyStore.getInstance("jks");
-						store.load(resource.getInputStream(), this.password);
-					}
-				}
-			}
-			RSAPrivateCrtKey key = (RSAPrivateCrtKey) store.getKey(alias, password);
-			RSAPublicKeySpec spec = new RSAPublicKeySpec(key.getModulus(), key.getPublicExponent());
-			PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(spec);
-			return new KeyPair(publicKey, key);
-		}
-		catch (Exception e) {
-			throw new IllegalStateException("Cannot load keys from store: " + resource, e);
-		}
-	}
+    public KeyPair getKeyPair(String alias, char[] password) {
+        try {
+            synchronized (lock) {
+                if (store == null) {
+
+                    InputStream inputStream = null;
+
+                    synchronized (lock) {
+                        try {
+                            store = KeyStore.getInstance("jks");
+                            inputStream = resource.getInputStream();
+                            store.load(inputStream, this.password);
+
+                        } finally {
+                            if (inputStream != null) {
+                                inputStream.close();
+                            }
+                        }
+                    }
+                }
+            }
+            RSAPrivateCrtKey key = (RSAPrivateCrtKey) store.getKey(alias, password);
+            RSAPublicKeySpec spec = new RSAPublicKeySpec(key.getModulus(), key.getPublicExponent());
+            PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(spec);
+            return new KeyPair(publicKey, key);
+        } catch (Exception e) {
+            throw new IllegalStateException("Cannot load keys from store: " + resource, e);
+        }
+    }
 
 }
