9c09209f1078b83026f52607ca2282dd8928dcac
Marcelo Barros <marcelopbarros@users.noreply.github.com>
2019-09-02 17:19:50 -0300

    Improves comparison of strings in DefaultRedirectResolver
    Fixes gh-1745

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/DefaultRedirectResolver.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/DefaultRedirectResolver.java
@@ -175,12 +175,10 @@ public class DefaultRedirectResolver implements RedirectResolver {
 	 * @return true if strings are equal, false otherwise
 	 */
 	private boolean isEqual(String str1, String str2) {
-		if (StringUtils.isEmpty(str1) && StringUtils.isEmpty(str2)) {
-			return true;
-		} else if (!StringUtils.isEmpty(str1)) {
-			return str1.equals(str2);
+		if (StringUtils.isEmpty(str1)) {
+			return StringUtils.isEmpty(str2);
 		} else {
-			return false;
+			return str1.equals(str2);
 		}
 	}
 
