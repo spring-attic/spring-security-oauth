cb9e8649d0e94a9b5d3f611056a272da7e4fc842
Keesun Baik <whiteship2000@gmail.com>
2020-04-03 11:50:56 -0700

    Fix NPE when refresh_token is not provided
    The NullPointerException can occure when the grant_type is refersh_token
    but the refresh_token itself is not provided. And the response code
    would be 500 as it is a server error.
    
    With this fix, it will return 4xx as it is one of the client side error
    that missed providing required parameter, refresh_token.

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/TokenEndpoint.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/TokenEndpoint.java
@@ -129,6 +129,9 @@ public class TokenEndpoint extends AbstractEndpoint {
 		}
 
 		if (isRefreshTokenRequest(parameters)) {
+			if (parameters.get("refresh_token") == null) {
+				throw new InvalidRequestException("Refresh token should be exists. Please check if refresh_token parameter is provided.");
+			}
 			// A refresh token has its own default scopes, so we should ignore any added by the factory here.
 			tokenRequest.setScope(OAuth2Utils.parseParameterList(parameters.get(OAuth2Utils.SCOPE)));
 		}
@@ -200,7 +203,7 @@ public class TokenEndpoint extends AbstractEndpoint {
 	}
 
 	private boolean isRefreshTokenRequest(Map<String, String> parameters) {
-		return "refresh_token".equals(parameters.get("grant_type")) && parameters.get("refresh_token") != null;
+		return "refresh_token".equals(parameters.get("grant_type"));
 	}
 
 	private boolean isAuthCodeRequest(Map<String, String> parameters) {
