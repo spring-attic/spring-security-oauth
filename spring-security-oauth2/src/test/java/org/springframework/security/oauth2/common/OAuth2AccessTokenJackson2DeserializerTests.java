44c85615e53f05ffccafa2a5b33e3bd2b8416a15
Alexandr Latushkin <alexandr.latushkin@gmail.com>
2020-05-08 21:39:18 +0300

    Fix #1172 Treat "expires_in: 0" as "not expired"

--- spring-security-oauth2/src/test/java/org/springframework/security/oauth2/common/OAuth2AccessTokenJackson2DeserializerTests.java
+++ spring-security-oauth2/src/test/java/org/springframework/security/oauth2/common/OAuth2AccessTokenJackson2DeserializerTests.java
@@ -27,7 +27,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertFalse;
 
-
 /**
  * Tests deserialization of an {@link org.springframework.security.oauth2.common.OAuth2AccessToken} using jackson.
  *
@@ -43,41 +42,71 @@ public class OAuth2AccessTokenJackson2DeserializerTests extends BaseOAuth2Access
         mapper = new ObjectMapper();
     }
 
-    @Test
-    public void readValueNoRefresh() throws JsonGenerationException, JsonMappingException, IOException {
-        accessToken.setRefreshToken(null);
-        accessToken.setScope(null);
-        OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_NOREFRESH, OAuth2AccessToken.class);
-        assertTokenEquals(accessToken, actual);
-    }
+	@Test
+	public void readValueNoRefresh() throws JsonGenerationException, JsonMappingException, IOException {
+		accessToken.setRefreshToken(null);
+		accessToken.setScope(null);
+		OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_NOREFRESH, OAuth2AccessToken.class);
+		assertTokenEquals(accessToken,actual);
+	}
 
-    @Test
-    public void readValueWithRefresh() throws JsonGenerationException, JsonMappingException, IOException {
-        accessToken.setScope(null);
-        OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_NOSCOPE, OAuth2AccessToken.class);
-        assertTokenEquals(accessToken, actual);
-    }
+	@Test
+	public void readValueWithRefresh() throws JsonGenerationException, JsonMappingException, IOException {
+		accessToken.setScope(null);
+		OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_NOSCOPE, OAuth2AccessToken.class);
+		assertTokenEquals(accessToken,actual);
+	}
 
-    @Test
-    public void readValueWithSingleScopes() throws JsonGenerationException, JsonMappingException, IOException {
-        accessToken.getScope().remove(accessToken.getScope().iterator().next());
-        OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_SINGLESCOPE, OAuth2AccessToken.class);
-        assertTokenEquals(accessToken, actual);
-    }
+	@Test
+	public void readValueWithSingleScopes() throws JsonGenerationException, JsonMappingException, IOException {
+		accessToken.getScope().remove(accessToken.getScope().iterator().next());
+		OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_SINGLESCOPE, OAuth2AccessToken.class);
+		assertTokenEquals(accessToken,actual);
+	}
 
-    @Test
-    public void readValueWithEmptyStringScope() throws JsonGenerationException, JsonMappingException, IOException {
-        accessToken.setScope(new HashSet<String>());
-        OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_EMPTYSCOPE, OAuth2AccessToken.class);
-        assertTokenEquals(accessToken, actual);
-    }
+	@Test
+	public void readValueWithEmptyStringScope() throws JsonGenerationException, JsonMappingException, IOException {
+		accessToken.setScope(new HashSet<String>());
+		OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_EMPTYSCOPE, OAuth2AccessToken.class);
+		assertTokenEquals(accessToken, actual);
+	}
 
-    @Test
-    public void readValueWithBrokenExpiresIn() throws JsonGenerationException, JsonMappingException, IOException {
-        accessToken.setScope(new HashSet<String>());
-        OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_BROKENEXPIRES, OAuth2AccessToken.class);
-        assertTokenEquals(accessToken, actual);
-    }
+	@Test
+	public void readValueWithBrokenExpiresIn() throws JsonGenerationException, JsonMappingException, IOException {
+		accessToken.setScope(new HashSet<String>());
+		OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_BROKENEXPIRES, OAuth2AccessToken.class);
+		assertTokenEquals(accessToken, actual);
+	}
+
+	@Test
+	public void readValueWithMultiScopes() throws Exception {
+		OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_MULTISCOPE, OAuth2AccessToken.class);
+		assertTokenEquals(accessToken,actual);
+	}
+
+	@Test
+	public void readValueWithArrayScopes() throws Exception {
+		OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_ARRAYSCOPE, OAuth2AccessToken.class);
+		assertTokenEquals(accessToken, actual);
+	}
+
+	@Test
+	public void readValueWithMac() throws Exception {
+		accessToken.setTokenType("mac");
+		String encodedToken = ACCESS_TOKEN_MULTISCOPE.replace("bearer", accessToken.getTokenType());
+		OAuth2AccessToken actual = mapper.readValue(encodedToken, OAuth2AccessToken.class);
+		assertTokenEquals(accessToken,actual);
+	}
+
+	@Test
+	public void readValueWithAdditionalInformation() throws Exception {
+		OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_ADDITIONAL_INFO, OAuth2AccessToken.class);
+		accessToken.setAdditionalInformation(additionalInformation);
+		accessToken.setRefreshToken(null);
+		accessToken.setScope(null);
+		accessToken.setExpiration(null);
+		assertTokenEquals(accessToken,actual);
+	}
 
     @Test
     public void readValueWithZeroExpiresAsNotExpired() throws JsonGenerationException, JsonMappingException, IOException {
@@ -85,53 +114,25 @@ public class OAuth2AccessTokenJackson2DeserializerTests extends BaseOAuth2Access
         assertFalse("Token with expires_in:0 must be treated as not expired.", actual.isExpired());
     }
 
-    @Test
-    public void readValueWithMultiScopes() throws Exception {
-        OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_MULTISCOPE, OAuth2AccessToken.class);
-        assertTokenEquals(accessToken, actual);
-    }
+	private static void assertTokenEquals(OAuth2AccessToken expected, OAuth2AccessToken actual) {
+		assertEquals(expected.getTokenType(), actual.getTokenType());
+		assertEquals(expected.getValue(), actual.getValue());
 
-    @Test
-    public void readValueWithArrayScopes() throws Exception {
-        OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_ARRAYSCOPE, OAuth2AccessToken.class);
-        assertTokenEquals(accessToken, actual);
-    }
-
-    @Test
-    public void readValueWithMac() throws Exception {
-        accessToken.setTokenType("mac");
-        String encodedToken = ACCESS_TOKEN_MULTISCOPE.replace("bearer", accessToken.getTokenType());
-        OAuth2AccessToken actual = mapper.readValue(encodedToken, OAuth2AccessToken.class);
-        assertTokenEquals(accessToken, actual);
-    }
-
-    @Test
-    public void readValueWithAdditionalInformation() throws Exception {
-        OAuth2AccessToken actual = mapper.readValue(ACCESS_TOKEN_ADDITIONAL_INFO, OAuth2AccessToken.class);
-        accessToken.setAdditionalInformation(additionalInformation);
-        accessToken.setRefreshToken(null);
-        accessToken.setScope(null);
-        accessToken.setExpiration(null);
-        assertTokenEquals(accessToken, actual);
-    }
-
-    private static void assertTokenEquals(OAuth2AccessToken expected, OAuth2AccessToken actual) {
-        assertEquals(expected.getTokenType(), actual.getTokenType());
-        assertEquals(expected.getValue(), actual.getValue());
-
-        OAuth2RefreshToken expectedRefreshToken = expected.getRefreshToken();
-        if (expectedRefreshToken == null) {
-            assertNull(actual.getRefreshToken());
-        } else {
-            assertEquals(expectedRefreshToken.getValue(), actual.getRefreshToken().getValue());
-        }
-        assertEquals(expected.getScope(), actual.getScope());
-        Date expectedExpiration = expected.getExpiration();
-        if (expectedExpiration == null) {
-            assertNull(actual.getExpiration());
-        } else {
-            assertEquals(expectedExpiration.getTime(), actual.getExpiration().getTime());
-        }
-        assertEquals(expected.getAdditionalInformation(), actual.getAdditionalInformation());
-    }
+		OAuth2RefreshToken expectedRefreshToken = expected.getRefreshToken();
+		if (expectedRefreshToken == null) {
+			assertNull(actual.getRefreshToken());
+		}
+		else {
+			assertEquals(expectedRefreshToken.getValue(), actual.getRefreshToken().getValue());
+		}
+		assertEquals(expected.getScope(), actual.getScope());
+		Date expectedExpiration = expected.getExpiration();
+		if (expectedExpiration == null) {
+			assertNull(actual.getExpiration());
+		}
+		else {
+			assertEquals(expectedExpiration.getTime(), actual.getExpiration().getTime());
+		}
+		assertEquals(expected.getAdditionalInformation(), actual.getAdditionalInformation());
+	}
 }
